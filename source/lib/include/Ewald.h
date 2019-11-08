acb29fabafd87b919f558feb26591905ec72b5a6
Han Wang <wang_han@iapcm.ac.cn>
2019-11-08 23:08:01 +0800

    multi-threading for ewald

--- source/lib/include/Ewald.h
+++ source/lib/include/Ewald.h
@@ -2,6 +2,7 @@
 
 #include<algorithm>
 #include<cassert>
+#include<omp.h>
 
 #include "SimulationRegion.h"
 
@@ -122,77 +123,115 @@ EwaldReciprocal(VALUETYPE &			ener,
   ener = 0;
   fill(force.begin(), force.end(), static_cast<VALUETYPE>(0));
   fill(virial.begin(), virial.end(), static_cast<VALUETYPE>(0));
-  
+
+  // number of threads
+  int nthreads = 1;
+#pragma omp parallel 
+  {
+    if (0 == omp_get_thread_num()) {
+      nthreads = omp_get_num_threads();
+    }
+  }
+
+  // K grid
   vector<int> KK(3);
   int totK = 1;
   cmpt_k<VALUETYPE>(KK, region, param);
   for (int dd = 0; dd < 3; ++dd){
     totK *= (KK[dd]+1);
   }  
+  int stride[3];
+  for (int dd = 0; dd < 3; ++dd) stride[dd] = KK[dd]+1;
   
   // compute the sq
+  vector<vector<VALUETYPE> > thread_sqr(nthreads), thread_sqi(nthreads);
+  for (int ii = 0; ii < nthreads; ++ii){
+    thread_sqr[ii].resize(totK, static_cast<VALUETYPE>(0));
+    thread_sqi[ii].resize(totK, static_cast<VALUETYPE>(0));
+  }  
+  // firstly loop over particles then loop over m
+#pragma omp parallel for num_threads(nthreads)
+  for (int ii = 0; ii < natoms; ++ii){
+    int thread_id = omp_get_thread_num();
+    double ir[3];
+    region.phys2Inter(ir, &coord[ii*3]);
+    for (int mm0 = -KK[0]/2; mm0 <= KK[0]/2; ++mm0){
+      double mr[3];
+      mr[0] = ir[0] * mm0;      
+      int shift0 = (mm0 + KK[0]/2) * stride[1] * stride[2];
+      for (int mm1 = -KK[1]/2; mm1 <= KK[1]/2; ++mm1){
+	mr[1] = ir[1] * mm1;
+	int shift1 = (mm1 + KK[1]/2) * stride[2];
+	for (int mm2 = -KK[2]/2; mm2 <= KK[2]/2; ++mm2){
+	  if (mm0 == 0 && mm1 == 0 && mm2 == 0) continue;
+	  int mc = shift0 + shift1 + mm2 + KK[2]/2;
+	  mr[2] = ir[2] * mm2;
+	  double mdotr = 2. * M_PI * (mr[0]+mr[1]+mr[2]);
+	  thread_sqr[thread_id][mc] += charge[ii] * cos(mdotr);
+	  thread_sqi[thread_id][mc] += charge[ii] * sin(mdotr);
+	}
+      }
+    }
+  }
   VALUETYPE * sqr = new VALUETYPE[totK];
   VALUETYPE * sqi = new VALUETYPE[totK];
   for (int ii = 0; ii < totK; ++ii){
     sqr[ii] = static_cast<VALUETYPE>(0);
     sqi[ii] = static_cast<VALUETYPE>(0);
-  }
-  // firstly loop over particles then loop over m
-  int mm[3];
-  for (int ii = 0; ii < natoms; ++ii){
-    double ir[3];
-    region.phys2Inter(ir, &coord[ii*3]);
-    double mr[3];
-    int mc = 0;
-    for (mm[0] = -KK[0]/2; mm[0] <= KK[0]/2; ++mm[0]){
-      mr[0] = ir[0] * mm[0];
-      for (mm[1] = -KK[1]/2; mm[1] <= KK[1]/2; ++mm[1]){
-	mr[1] = ir[1] * mm[1];
-	for (mm[2] = -KK[2]/2; mm[2] <= KK[2]/2; ++mm[2]){
-	  if (mm[0] == 0 && mm[1] == 0 && mm[2] == 0) continue;
-	  mr[2] = ir[2] * mm[2];
-	  double mdotr = 2. * M_PI * (mr[0]+mr[1]+mr[2]);
-	  sqr[mc] += charge[ii] * cos(mdotr);
-	  sqi[mc] += charge[ii] * sin(mdotr);
-	  ++mc;
-	}
-      }
+    for (int jj = 0; jj < nthreads; ++jj){
+      sqr[ii] += thread_sqr[jj][ii];
+      sqi[ii] += thread_sqi[jj][ii];
     }
-  }
+  }  
+
+  // get rbox
   VALUETYPE rec_box[9];
   const double * rec_box_ = region.getRecBoxTensor();
   for (int ii = 0; ii < 9; ++ii){
     rec_box[ii] = static_cast<VALUETYPE>(rec_box_[ii]);
   }
+  
+  vector<VALUETYPE> thread_ener(nthreads, 0.);
+  vector<vector<VALUETYPE> > thread_force(nthreads);
+  vector<vector<VALUETYPE> > thread_virial(nthreads);
+  for (int ii = 0; ii < nthreads; ++ii){
+    thread_force[ii].resize(natoms * 3, 0.);
+    thread_virial[ii].resize(9, 0.);
+  }
   // calculate ener, force and virial
-  // firstly loop over particles then loop over m
-  int mc = 0;
-  for (mm[0] = -KK[0]/2; mm[0] <= KK[0]/2; ++mm[0]){
-    for (mm[1] = -KK[1]/2; mm[1] <= KK[1]/2; ++mm[1]){
-      for (mm[2] = -KK[2]/2; mm[2] <= KK[2]/2; ++mm[2]){
-	if (mm[0] == 0 && mm[1] == 0 && mm[2] == 0) continue;
+  // firstly loop over particles then loop over m  
+#pragma omp parallel for num_threads(nthreads)
+  for (int mm0 = -KK[0]/2; mm0 <= KK[0]/2; ++mm0){
+    int thread_id = omp_get_thread_num();
+    int shift0 = (mm0 + KK[0]/2) * stride[1] * stride[2];
+    for (int mm1 = -KK[1]/2; mm1 <= KK[1]/2; ++mm1){
+      int shift1 = (mm1 + KK[1]/2) * stride[2];
+      for (int mm2 = -KK[2]/2; mm2 <= KK[2]/2; ++mm2){
+	if (mm0 == 0 && mm1 == 0 && mm2 == 0) continue;
+	int mc = shift0 + shift1 + mm2 + KK[2]/2;
 	// \bm m and \vert m \vert^2
 	VALUETYPE rm[3] = {0,0,0};	  
-	for (int dd = 0; dd < 3; ++dd){
-	  rm[0] += mm[dd] * rec_box[0*3+dd];
-	  rm[1] += mm[dd] * rec_box[1*3+dd];
-	  rm[2] += mm[dd] * rec_box[2*3+dd];
-	  // rm[0] += mm[dd] * rec_box[dd*3+0];
-	  // rm[1] += mm[dd] * rec_box[dd*3+1];
-	  // rm[2] += mm[dd] * rec_box[dd*3+2];
-	}
-	VALUETYPE mm2 = rm[0] * rm[0] + rm[1] * rm[1] + rm[2] * rm[2];
+	rm[0] += mm0 * rec_box[0*3+0];
+	rm[1] += mm0 * rec_box[1*3+0];
+	rm[2] += mm0 * rec_box[2*3+0];
+	rm[0] += mm1 * rec_box[0*3+1];
+	rm[1] += mm1 * rec_box[1*3+1];
+	rm[2] += mm1 * rec_box[2*3+1];
+	rm[0] += mm2 * rec_box[0*3+2];
+	rm[1] += mm2 * rec_box[1*3+2];
+	rm[2] += mm2 * rec_box[2*3+2];
+	VALUETYPE nmm2 = rm[0] * rm[0] + rm[1] * rm[1] + rm[2] * rm[2];
 	// energy
-	VALUETYPE expmm2 = exp(- M_PI * M_PI * mm2 / (param.beta * param.beta)) / mm2;
-	VALUETYPE eincr = expmm2 * (sqr[mc] * sqr[mc] + sqi[mc] * sqi[mc]);
-	ener += eincr;
+	VALUETYPE expnmm2 = exp(- M_PI * M_PI * nmm2 / (param.beta * param.beta)) / nmm2;
+	VALUETYPE eincr = expnmm2 * (sqr[mc] * sqr[mc] + sqi[mc] * sqi[mc]);
+	thread_ener[thread_id] += eincr;
 	// virial
-	VALUETYPE vpref = -2. * (1. + M_PI * M_PI * mm2 / (param.beta * param.beta)) / mm2;
+	VALUETYPE vpref = -2. * (1. + M_PI * M_PI * nmm2 / (param.beta * param.beta)) / nmm2;
 	for (int dd0 = 0; dd0 < 3; ++dd0){
 	  for (int dd1 = 0; dd1 < 3; ++dd1){	    
 	    VALUETYPE tmp = vpref * rm[dd0] * rm[dd1];
 	    if (dd0 == dd1) tmp += 1;
-	    virial[dd0*3+dd1] += eincr * tmp;
+	    thread_virial[thread_id][dd0*3+dd1] += eincr * tmp;
 	  }
 	}
 	// force
@@ -200,15 +239,29 @@ EwaldReciprocal(VALUETYPE &			ener,
 	  VALUETYPE mdotr = - 2. * M_PI * (coord[ii*3+0]*rm[0] + coord[ii*3+1]*rm[1] + coord[ii*3+2]*rm[2]);
 	  VALUETYPE tmpr = charge[ii] * cos(mdotr);
 	  VALUETYPE tmpi = charge[ii] * sin(mdotr);
-	  VALUETYPE cc = 4. * M_PI * (tmpr * sqi[mc] + tmpi * sqr[mc]) * expmm2;
-	  force[ii*3+0] -= rm[0] * cc;
-	  force[ii*3+1] -= rm[1] * cc;
-	  force[ii*3+2] -= rm[2] * cc;
+	  VALUETYPE cc = 4. * M_PI * (tmpr * sqi[mc] + tmpi * sqr[mc]) * expnmm2;
+	  thread_force[thread_id][ii*3+0] -= rm[0] * cc;
+	  thread_force[thread_id][ii*3+1] -= rm[1] * cc;
+	  thread_force[thread_id][ii*3+2] -= rm[2] * cc;
 	}	  
-	++mc;
       }
     }
   }
+  // reduce thread results
+  for (int ii = 0; ii < nthreads; ++ii){
+    ener += thread_ener[ii];
+  }
+  for (int jj = 0; jj < 9; ++jj){
+    for (int ii = 0; ii < nthreads; ++ii){
+      virial[jj] += thread_virial[ii][jj];
+    }
+  }
+  for (int jj = 0; jj < natoms * 3; ++jj){
+    for (int ii = 0; ii < nthreads; ++ii){
+      force[jj] += thread_force[ii][jj];
+    }
+  }
+
   VALUETYPE vol = static_cast<VALUETYPE>(region.getVolume());
   ener /= 2 * M_PI * vol;
   ener *= ElectrostaticConvertion;
