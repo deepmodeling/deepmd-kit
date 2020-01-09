04a1f1c7ef8a183092d1330d1e846dead613e0f5
Lu <Eric@LuDhMacBook-Pro.local>
2020-01-09 19:36:55 +0800

    delete unused functions

--- source/op/cuda/descrpt_se_r.cu
+++ source/op/cuda/descrpt_se_r.cu
@@ -41,20 +41,6 @@ inline void cudaAssert(cudaError_t code, const char *file, int line, bool abort=
     }
 }
 
-#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 600
-static __inline__ __device__ double atomicAdd(double* address, double val) {
-    unsigned long long int* address_as_ull = (unsigned long long int*)address;
-    unsigned long long int old = *address_as_ull, assumed;
-    do {
-        assumed = old;
-        old = atomicCAS(address_as_ull, assumed,
-                __double_as_longlong(val + __longlong_as_double(assumed)));
-    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN) } while (assumed != old);
-    } while (assumed != old);
-    return __longlong_as_double(old);
-}
-#endif
-
 template <
     typename    Key,
     int         BLOCK_THREADS,
