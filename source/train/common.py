6a1b1fbcbc710121e2a3e1405a52f536996420d0
GeiduanLiu <61015788+GeiduanLiu@users.noreply.github.com>
2020-02-19 17:25:26 +0800

    Update common.py

--- source/train/common.py
+++ source/train/common.py
@@ -1,6 +1,19 @@
 import os,warnings,fnmatch
 import numpy as np
+import math
 from deepmd.env import tf
+
+def gelu(x):
+    """Gaussian Error Linear Unit.
+    This is a smoother version of the RELU.
+    Original paper: https://arxiv.org/abs/1606.08415
+    Args:
+    x: float Tensor to perform activation.
+    Returns:
+    `x` with the GELU activation applied.
+    """
+    cdf = 0.5 * (1.0 + tf.tanh((math.sqrt(2 / math.pi) * (x + 0.044715 * tf.pow(x, 3)))))
+    return x * cdf
 
 data_requirement = {}
 activation_fn_dict = {
@@ -8,7 +21,8 @@ activation_fn_dict = {
     "relu6": tf.nn.relu6,
     "softplus": tf.nn.softplus,
     "sigmoid": tf.sigmoid,
-    "tanh": tf.nn.tanh
+    "tanh": tf.nn.tanh,
+    "gelu": gelu
 }
 def add_data_requirement(key, 
                          ndof, 
