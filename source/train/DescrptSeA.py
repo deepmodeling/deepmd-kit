886ca0970f5baf49280bb6b1b1cc2ac08c4408ac
GeiduanLiu <61015788+GeiduanLiu@users.noreply.github.com>
2020-02-23 00:30:09 +0800

    Update DescrptSeA.py

--- source/train/DescrptSeA.py
+++ source/train/DescrptSeA.py
@@ -1,6 +1,6 @@
 import numpy as np
 from deepmd.env import tf
-from deepmd.common import ClassArg, get_activation_func， get_precision_func
+from deepmd.common import ClassArg, get_activation_func, get_precision_func
 from deepmd.RunOptions import global_tf_float_precision
 from deepmd.RunOptions import global_np_float_precision
 from deepmd.env import op_module
