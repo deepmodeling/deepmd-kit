d77ffd3c29dc34f463644209550eed87d218b9a3
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-10-17 19:05:50 -0400

    remove useless code

--- setup.py
+++ setup.py
@@ -18,9 +18,6 @@ except ImportError:
 install_requires=['numpy', 'scipy', 'pyyaml']
 setup_requires=['setuptools_scm', 'scikit-build']
 
-def find_tf(path):
-    return importlib.machinery.FileFinder(path).find_spec("tensorflow")
-
 tf_spec = importlib.util.find_spec("tensorflow")
 if tf_spec:
     tf_install_dir = tf_spec.submodule_search_locations[1]
