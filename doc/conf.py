106ab31b4ac028a7bec1bbee92b88e510fd93b74
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-11-21 19:44:42 -0500

    update docs

--- doc/conf.py
+++ doc/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'deepmd-kit'
-copyright = '2020, dp'
-author = 'dp'
+project = 'DeePMD-kit'
+copyright = '2020, Deep Potential'
+author = 'Deep Potential'
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,6 +28,9 @@ author = 'dp'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'recommonmark',
+    "sphinx_rtd_theme",
+    'sphinx.ext.autosummary'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,9 +47,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+autodoc_default_flags = ['members']
+autosummary_generate = True
--- doc/index.rst
+++ doc/index.rst
@@ -3,13 +3,19 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to deepmd-kit's documentation!
+DeePMD-kit's documentation
 ======================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+   
+   install
+   use-deepmd-kit
+   train-input
+   lammps-pair-style-deepmd
+   api
 
 
 Indices and tables
