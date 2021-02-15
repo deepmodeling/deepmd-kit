ae73f41489d12d97253e171d824318a2723dc260
Marián Rynik <marian.rynik@outlook.sk>
2021-02-15 22:26:00 +0100

    fix bugs after ops move

--- deepmd/op/__init__.py
+++ deepmd/op/__init__.py
@@ -2,17 +2,20 @@
 
 from pathlib import Path
 import importlib
+import logging
 
 NOT_LOADABLE = ("__init__.py")
 PACKAGE_BASE = "deepmd.op"
+
+log = logging.getLogger(__name__)
 
 
 def import_ops():
     """Import all custom TF ops that are present in this submodule."""
     for module_file in Path(__file__).parent.glob("*.py"):
         if module_file.name not in NOT_LOADABLE:
-            module_name = module_file.stem
+            module_name = f".{module_file.stem}"
+            log.debug(f"importing op module: {module_name}")
             importlib.import_module(module_name, PACKAGE_BASE)
-
 
 import_ops()
