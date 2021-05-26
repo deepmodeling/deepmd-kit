c711e3f5518fd979868cc58786d0e44b47fafad9
tuoping <abby@DESKTOP-LV5KL0D.localdomain>
2021-05-26 10:08:43 +0800

    Edit conf.py to enable doxygen on readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -53,6 +53,31 @@ project = 'DeePMD-kit'
 copyright = '2020, Deep Potential'
 author = 'Deep Potential'
 
+def run_doxygen(folder):
+    """Run the doxygen make command in the designated folder"""
+
+    try:
+        retcode = subprocess.call("cd %s; doxygen Doxyfile" % folder, shell=True)
+        if retcode < 0:
+            sys.stderr.write("doxygen terminated by signal %s" % (-retcode))
+    except OSError as e:
+        sys.stderr.write("doxygen execution failed: %s" % e)
+
+
+def generate_doxygen_xml(app):
+    """Run the doxygen make commands if we're on the ReadTheDocs server"""
+
+    read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+    if read_the_docs_build:
+        run_doxygen("./")
+    else:
+        subprocess.call("doxygen Doxyfile", shell=True)
+
+def setup(app):
+
+    # Add hook for building doxygen xml when needed
+    app.connect("builder-inited", generate_doxygen_xml)
 
 # -- General configuration ---------------------------------------------------
 
@@ -66,7 +91,6 @@ author = 'Deep Potential'
 #     'sphinx_markdown_tables',
 #     'sphinx.ext.autosummary'
 # ]
-subprocess.call("doxygen Doxyfile", shell=True)
 
 mkindex("troubleshooting")
 mkindex("development")
