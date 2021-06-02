a631c11515e4cdb8b9c90c6cee1292fd046b7e47
Han Wang <wang_han@iapcm.ac.cn>
2021-06-02 15:40:34 +0800

    fix bugs in wfc

--- deepmd/fit/wfc.py
+++ deepmd/fit/wfc.py
@@ -5,7 +5,7 @@ from typing import Tuple, List
 from deepmd.env import tf
 from deepmd.common import ClassArg, add_data_requirement, get_activation_func, get_precision, ACTIVATION_FN_DICT, PRECISION_DICT, docstring_parameter
 from deepmd.utils.argcheck import list_to_doc
-from deepmd.utils.network import one_layer
+from deepmd.utils.network import one_layer, one_layer_rand_seed_shift
 from deepmd.descriptor import DescrptLocFrame
 
 from deepmd.env import global_cvt_2_tf_float
@@ -27,13 +27,16 @@ class WFCFitting () :
                .add('sel_type',         [list,int],   default = [ii for ii in range(self.ntypes)], alias = 'wfc_type')\
                .add('seed',             int)\
                .add("activation_function", str, default = "tanh")\
-               .add('precision',           str,    default = "default")
+               .add('precision',           str,    default = "default")\
+               .add('uniform_seed',     bool, default = False)
         class_data = args.parse(jdata)
         self.n_neuron = class_data['neuron']
         self.resnet_dt = class_data['resnet_dt']
         self.wfc_numb = class_data['wfc_numb']
         self.sel_type = class_data['sel_type']
         self.seed = class_data['seed']
+        self.uniform_seed = class_data['uniform_seed']
+        self.seed_shift = one_layer_rand_seed_shift()
         self.fitting_activation_fn = get_activation_func(class_data["activation_function"])
         self.fitting_precision = get_precision(class_data['precision'])
         self.useBN = False
@@ -75,11 +78,13 @@ class WFCFitting () :
             layer = inputs_i
             for ii in range(0,len(self.n_neuron)) :
                 if ii >= 1 and self.n_neuron[ii] == self.n_neuron[ii-1] :
-                    layer+= one_layer(layer, self.n_neuron[ii], name='layer_'+str(ii)+'_type_'+str(type_i)+suffix, reuse=reuse, seed = self.seed, use_timestep = self.resnet_dt, activation_fn = self.fitting_activation_fn, precision = self.fitting_precision)
+                    layer+= one_layer(layer, self.n_neuron[ii], name='layer_'+str(ii)+'_type_'+str(type_i)+suffix, reuse=reuse, seed = self.seed, use_timestep = self.resnet_dt, activation_fn = self.fitting_activation_fn, precision = self.fitting_precision, uniform_seed = self.uniform_seed)
                 else :
-                    layer = one_layer(layer, self.n_neuron[ii], name='layer_'+str(ii)+'_type_'+str(type_i)+suffix, reuse=reuse, seed = self.seed, activation_fn = self.fitting_activation_fn, precision = self.fitting_precision)
+                    layer = one_layer(layer, self.n_neuron[ii], name='layer_'+str(ii)+'_type_'+str(type_i)+suffix, reuse=reuse, seed = self.seed, activation_fn = self.fitting_activation_fn, precision = self.fitting_precision, uniform_seed = self.uniform_seed)
+                if not self.uniform_seed : self.seed += self.seed_shift
             # (nframes x natoms) x (nwfc x 3)
-            final_layer = one_layer(layer, self.wfc_numb * 3, activation_fn = None, name='final_layer_type_'+str(type_i)+suffix, reuse=reuse, seed = self.seed, precision = self.fitting_precision)
+            final_layer = one_layer(layer, self.wfc_numb * 3, activation_fn = None, name='final_layer_type_'+str(type_i)+suffix, reuse=reuse, seed = self.seed, precision = self.fitting_precision, uniform_seed = self.uniform_seed)
+            if not self.uniform_seed : self.seed += self.seed_shift
             # (nframes x natoms) x nwfc(wc) x 3(coord_local)
             final_layer = tf.reshape(final_layer, [tf.shape(inputs)[0] * natoms[2+type_i], self.wfc_numb, 3])
             # (nframes x natoms) x nwfc(wc) x 3(coord)
