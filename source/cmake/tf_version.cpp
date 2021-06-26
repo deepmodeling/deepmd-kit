b145ca336e63a6d2628d8cdd3970ac395e577b98
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-06-26 06:10:48 -0400

    throw a message if tf runtime is incompatible (#797)
    * throw a message if tf runtime is incompatible
    
    fix #557 and #796.
    
    * still raise if tf version is correct
    
    * detect TF_CXX11_ABI_FLAG
    
    * format codes
    
    * fix lint
    
    * move messages into the function
    
    * fix lint
    
    * fix lints

---
+++ source/cmake/tf_version.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "tensorflow/core/public/version.h"
+
+int main(int argc, char * argv[])
+{
+  // See https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/public/version.h
+  // TF_VERSION_STRING has been avaiable since TensorFlow v0.6
+  std::cout << TF_VERSION_STRING;
+  return 0;
+}
