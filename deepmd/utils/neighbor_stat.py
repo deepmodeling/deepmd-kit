04cc1b5ded18bca8db1524fa5e91e9cab6d106dd
Nick Lin <33555716+nicklin96@users.noreply.github.com>
2021-07-23 15:03:27 +0800

    Update neighbor_stat.py
    use log.warning instead of log.warn

--- deepmd/utils/neighbor_stat.py
+++ deepmd/utils/neighbor_stat.py
@@ -86,7 +86,7 @@ class NeighborStat():
                         dt = np.min(dt)              
                     else:
                         dt = self.rcut
-                        log.warn("Atoms with no neighbors found in %s. Please make sure it's what you expected."%jj)
+                        log.warning("Atoms with no neighbors found in %s. Please make sure it's what you expected."%jj)
                         
                     if dt < self.min_nbor_dist:
                         self.min_nbor_dist = dt
