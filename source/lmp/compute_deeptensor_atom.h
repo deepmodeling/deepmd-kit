b30a75ecab0b09e8dd2dacfc3727097a99362cd6
Han Wang <amcadmus@gmail.com>
2021-08-08 20:36:26 +0800

    add lammps compute style for atomic deep tensor (#927)
    * add lammps compute style for deep tensor
    
    * support the choice of floating point precision
    
    * update doc for deeptensor/atom
    
    Co-authored-by: Han Wang <wang_han@iapcm.ac.cn>

---
+++ source/lmp/compute_deeptensor_atom.h
@@ -0,0 +1,38 @@
+#ifdef COMPUTE_CLASS
+
+ComputeStyle(deeptensor/atom,ComputeDeeptensorAtom)
+
+#else
+
+#ifndef LMP_COMPUTE_DEEPTENSOR_ATOM_H
+#define LMP_COMPUTE_DEEPTENSOR_ATOM_H
+
+#include "compute.h"
+#include "pair_deepmd.h"
+#include "deepmd/DeepTensor.h"
+
+namespace LAMMPS_NS {
+
+class ComputeDeeptensorAtom : public Compute {
+ public:
+  ComputeDeeptensorAtom(class LAMMPS *, int, char **);
+  ~ComputeDeeptensorAtom();
+  void init();
+  void compute_peratom();
+  double memory_usage();
+  void init_list(int, class NeighList *);
+
+ private:
+  int nmax;
+  double **tensor;
+  PairDeepMD dp;
+  class NeighList *list;
+  deepmd::DeepTensor dt;
+  std::vector<int > sel_types;
+};
+
+}
+
+#endif
+#endif
+
