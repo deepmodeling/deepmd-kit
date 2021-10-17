fix single precision error

deepmd/descriptor/se_t.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import Tuple, List
 
 from deepmd.env import tf
-from deepmd.common import get_activation_func, get_precision, ACTIVATION_FN_DICT, PRECISION_DICT, docstring_parameter, get_np_precision
+from deepmd.common import get_activation_func, get_precision, ACTIVATION_FN_DICT, PRECISION_DICT, docstring_parameter
 from deepmd.utils.argcheck import list_to_doc
 from deepmd.env import GLOBAL_TF_FLOAT_PRECISION
 from deepmd.env import GLOBAL_NP_FLOAT_PRECISION
@@ -78,7 +78,6 @@ class DescrptSeT (DescrptSe):
         self.trainable = trainable
         self.filter_activation_fn = get_activation_func(activation_function)
         self.filter_precision = get_precision(precision)
-        self.filter_np_precision = get_np_precision(precision)
         # self.exclude_types = set()
         # for tt in exclude_types:
         #     assert(len(tt) == 2)
@@ -551,7 +550,7 @@ class DescrptSeT (DescrptSe):
                     if self.compress:
                         info = [self.lower, self.upper, self.upper * self.table_config[0], self.table_config[1], self.table_config[2], self.table_config[3]]
                         net = 'filter_' + str(type_i) + '_net_' + str(type_j)
-                        res_ij = op_module.tabulate_fusion_se_t(self.table.data[net].astype(self.filter_np_precision), info, ebd_env_ij, env_ij, last_layer_size = outputs_size[-1]) 
+                        res_ij = op_module.tabulate_fusion_se_t(tf.cast(self.table.data[net], self.filter_precision), info, ebd_env_ij, env_ij, last_layer_size = outputs_size[-1]) 
                     else:
                         # with (natom x nei_type_i x nei_type_j) x out_size
                         ebd_env_ij = embedding_net(ebd_env_ij, 