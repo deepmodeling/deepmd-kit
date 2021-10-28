d57a53a2161f7c8933b542898be357f24998d65d
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-10-28 19:50:41 -0400

    deprecate `numb_test` (#1249)
    * deprecate `numb_test`
    
    See #1243.
    
    * bugfix

--- deepmd/utils/argcheck.py
+++ deepmd/utils/argcheck.py
@@ -605,7 +605,6 @@ def training_args():  # ! modified by Ziyao: data configuration isolated.
     doc_seed = 'The random seed for getting frames from the training data set.'
     doc_disp_file = 'The file for printing learning curve.'
     doc_disp_freq = 'The frequency of printing learning curve.'
-    doc_numb_test = 'Number of frames used for the test during training.'
     doc_save_freq = 'The frequency of saving check point.'
     doc_save_ckpt = 'The file name of saving check point.'
     doc_disp_training = 'Displaying verbose information during training.'
@@ -626,7 +625,6 @@ def training_args():  # ! modified by Ziyao: data configuration isolated.
         Argument("seed", [int,None], optional=True, doc=doc_seed),
         Argument("disp_file", str, optional=True, default='lcurve.out', doc=doc_disp_file),
         Argument("disp_freq", int, optional=True, default=1000, doc=doc_disp_freq),
-        Argument("numb_test", [list,int,str], optional=True, default=1, doc=doc_numb_test),
         Argument("save_freq", int, optional=True, default=1000, doc=doc_save_freq),
         Argument("save_ckpt", str, optional=True, default='model.ckpt', doc=doc_save_ckpt),
         Argument("disp_training", bool, optional=True, default=True, doc=doc_disp_training),
