3d93fd00b5a87e21de732c6a540cc911f6c9b9aa
Denghui Lu <denghuilu@pku.edu.cn>
2021-11-23 08:35:33 +0800

    add embedding network dimension check of model compression (#1303)

--- deepmd/descriptor/se_a.py
+++ deepmd/descriptor/se_a.py
@@ -334,6 +334,14 @@ class DescrptSeA (DescrptSe):
         if (self.ntypes * self.ntypes - len(self.exclude_types) == 0):
             raise RuntimeError("empty embedding-net are not supported in model compression!")
 
+        for ii in range(len(self.filter_neuron) - 1):
+            if self.filter_neuron[ii] * 2 != self.filter_neuron[ii + 1]:
+                raise RecursionError(
+                    "Model Compression error: descriptor neuron [%s] is not supported by model compression! "
+                    "The size of the next layer of the neural network must be twice the size of the previous layer." 
+                    % ','.join([str(item) for item in self.filter_neuron])
+                )
+
         self.compress = True
         self.table = DPTabulate(
             self, self.filter_neuron, model_file, self.type_one_side, self.exclude_types, self.compress_activation_fn, suffix=suffix)
