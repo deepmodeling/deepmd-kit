6667e3d0189aba638db5a2d919e4617a6898f6db
dingye <dingye@westlake.edu.cn>
2021-12-01 16:56:59 +0800

    Synced with Github devel version.

--- deepmd/model/ener.py
+++ deepmd/model/ener.py
@@ -227,7 +227,6 @@ class EnerModel() :
         energy = tf.reduce_sum(global_cvt_2_ener_float(energy_raw), axis=1, name='o_energy'+suffix)
 
         
-        # TODO : add mask matrix input for prod_force_virial
         if hasattr(self.descrpt, 'descrpt_type') and self.descrpt.descrpt_type == "se_a_mask":
             force, virial, atom_virial \
                 = self.descrpt.prod_force_virial (atom_ener, natoms, input_dict["mask_matrix"])
