f2b5c2c75952ed4b761c6310a2944568f06e85fa
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-05-24 22:12:20 -0400

    prevent explicit slash in the path (#1713)
    Windows platform has different slashes..

--- deepmd/entrypoints/compress.py
+++ deepmd/entrypoints/compress.py
@@ -102,7 +102,7 @@ def compress(
         10 * step,
         int(frequency),
     ]
-    jdata["training"]["save_ckpt"] = "model-compression/model.ckpt"
+    jdata["training"]["save_ckpt"] = os.path.join("model-compression", "model.ckpt")
     jdata = update_deepmd_input(jdata)
     jdata = normalize(jdata)
 
