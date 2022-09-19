6944885937dba7c29868cdb03bfe3ccdac09fc5d
Denghui Lu <denghuilu@pku.edu.cn>
2022-09-19 11:44:28 +0800

    Use GeluCustom as operator name (#1918)
    This should avoid the conflicts related to the custom `Gelu` and
    Tensorflow's `Gelu`. Also Fix #1913.

--- deepmd/common.py
+++ deepmd/common.py
@@ -66,7 +66,7 @@ def gelu(x: tf.Tensor) -> tf.Tensor:
     Original paper
     https://arxiv.org/abs/1606.08415
     """
-    return op_module.gelu(x)
+    return op_module.gelu_custom(x)
 
 
 def gelu_tf(x: tf.Tensor) -> tf.Tensor:
@@ -94,7 +94,7 @@ def gelu_tf(x: tf.Tensor) -> tf.Tensor:
             return tensorflow.nn.gelu(x, approximate=True)
         except AttributeError:
             warnings.warn("TensorFlow does not provide an implementation of gelu, please upgrade your TensorFlow version. Fallback to the custom gelu operator.")
-            return op_module.gelu(x)
+            return op_module.gelu_custom(x)
     return (lambda x: gelu_wrapper(x))(x)
 
 # TODO this is not a good way to do things. This is some global variable to which
