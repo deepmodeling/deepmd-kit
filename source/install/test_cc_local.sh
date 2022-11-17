49c1c74fa93521f1ba9f822df1309e43f8485c17
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-11-16 19:10:24 -0500

    merge tests into the main cmake project

--- source/install/test_cc_local.sh
+++ source/install/test_cc_local.sh
@@ -2,50 +2,22 @@ set -e
 
 #------------------
 
-SCRIPT_PATH=$(dirname $(realpath -s $0))
-NPROC=$(nproc --all)
-
-#------------------
+echo "try to find tensorflow in ${tensorflow_root}"
 INSTALL_PREFIX=${SCRIPT_PATH}/../../dp_test
 BUILD_TMP_DIR=${SCRIPT_PATH}/../build_tests
 mkdir -p ${BUILD_TMP_DIR}
 cd ${BUILD_TMP_DIR}
-cmake ../lib/tests -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
+cmake -DINSTALL_TENSORFLOW=FALSE -DTENSORFLOW_ROOT=${tensorflow_root} -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DBUILD_TESTING:BOOL=TRUE ..
 make -j${NPROC}
 make install
 
 #------------------
-${INSTALL_PREFIX}/bin/runUnitTests
+# go to a subdirectory...
+# TODO: detect directory of graph files
+mkdir -p ${BUILD_TMP_DIR}/exec_tests
+cd ${BUILD_TMP_DIR}/exec_tests
 
-
-#------------------
-
-echo "try to find tensorflow in ${tensorflow_root}"
-BUILD_TMP_DIR=${SCRIPT_PATH}/../build_cc_tests
-INSTALL_PREFIX=${SCRIPT_PATH}/../../dp_test_cc
-mkdir -p ${BUILD_TMP_DIR}
-cd ${BUILD_TMP_DIR}
-cmake -DINSTALL_TENSORFLOW=FALSE -DTENSORFLOW_ROOT=${tensorflow_root} ../api_cc/tests -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
-make -j${NPROC}
-make install
-
-#------------------
-cd ${SCRIPT_PATH}/../api_cc/tests
-${INSTALL_PREFIX}/bin/runUnitTests
-${INSTALL_PREFIX}/bin/runUnitTests_low
-
-#------------------
-
-BUILD_TMP_DIR=${SCRIPT_PATH}/../build_c_tests
-INSTALL_PREFIX=${SCRIPT_PATH}/../../dp_test_c
-mkdir -p ${BUILD_TMP_DIR}
-mkdir -p ${INSTALL_PREFIX}
-cd ${BUILD_TMP_DIR}
-cmake -DINSTALL_TENSORFLOW=FALSE -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DTENSORFLOW_ROOT=${INSTALL_PREFIX} ../api_c/tests
-make -j${NPROC}
-make install
-
-#------------------
-cd ${SCRIPT_PATH}/../api_c/tests
-${INSTALL_PREFIX}/bin/runUnitTests
-
+${INSTALL_PREFIX}/bin/runUnitTests_lib
+${INSTALL_PREFIX}/bin/runUnitTests_cc
+${INSTALL_PREFIX}/bin/runUnitTests_cc_low
+${INSTALL_PREFIX}/bin/runUnitTests_c
