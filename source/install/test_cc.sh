37c69bc545d9ef4eb25021eed90a1dab1dd431f0
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-11-17 20:19:24 -0500

    refactor: merge tests into the main cmake project (#2102)
    Merge three test projects (lib, api_cc, api_c) into the main cmake
    project to avoid duplicated codes and duplicated build time.
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/install/test_cc.sh
+++ source/install/test_cc.sh
@@ -11,42 +11,17 @@ INSTALL_PREFIX=${SCRIPT_PATH}/../../dp_test
 BUILD_TMP_DIR=${SCRIPT_PATH}/../build_tests
 mkdir -p ${BUILD_TMP_DIR}
 cd ${BUILD_TMP_DIR}
-cmake ../lib/tests -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
+cmake -DINSTALL_TENSORFLOW=TRUE -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DTENSORFLOW_ROOT=${INSTALL_PREFIX} -DBUILD_TESTING:BOOL=TRUE -DLAMMPS_VERSION=stable_23Jun2022_update2 ..
 make -j${NPROC}
 make install
 
 #------------------
-${INSTALL_PREFIX}/bin/runUnitTests
+# go to a subdirectory...
+# TODO: detect directory of graph files
+mkdir -p ${BUILD_TMP_DIR}/exec_tests
+cd ${BUILD_TMP_DIR}/exec_tests
 
-
-#------------------
-
-BUILD_TMP_DIR=${SCRIPT_PATH}/../build_cc_tests
-INSTALL_PREFIX=${SCRIPT_PATH}/../../dp_test_cc
-mkdir -p ${BUILD_TMP_DIR}
-mkdir -p ${INSTALL_PREFIX}
-cd ${BUILD_TMP_DIR}
-cmake -DINSTALL_TENSORFLOW=TRUE -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DTENSORFLOW_ROOT=${INSTALL_PREFIX} ../api_cc/tests
-make -j${NPROC}
-make install
-
-#------------------
-cd ${SCRIPT_PATH}/../api_cc/tests
-${INSTALL_PREFIX}/bin/runUnitTests
-${INSTALL_PREFIX}/bin/runUnitTests_low
-
-#------------------
-
-BUILD_TMP_DIR=${SCRIPT_PATH}/../build_c_tests
-INSTALL_PREFIX=${SCRIPT_PATH}/../../dp_test_c
-mkdir -p ${BUILD_TMP_DIR}
-mkdir -p ${INSTALL_PREFIX}
-cd ${BUILD_TMP_DIR}
-cmake -DINSTALL_TENSORFLOW=TRUE -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DTENSORFLOW_ROOT=${INSTALL_PREFIX} ../api_c/tests
-make -j${NPROC}
-make install
-
-#------------------
-cd ${SCRIPT_PATH}/../api_c/tests
-${INSTALL_PREFIX}/bin/runUnitTests
-
+${INSTALL_PREFIX}/bin/runUnitTests_lib
+${INSTALL_PREFIX}/bin/runUnitTests_cc
+${INSTALL_PREFIX}/bin/runUnitTests_cc_low
+${INSTALL_PREFIX}/bin/runUnitTests_c
