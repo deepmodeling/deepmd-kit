4852bd8baacadc7a7869be9c25f2a399171e0787
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-11-23 05:13:17 -0500

    add missing headers
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/api_cc/tests/test_utils.h
+++ source/api_cc/tests/test_utils.h
@@ -2,6 +2,8 @@
 #include <cmath>
 #include "gtest/gtest.h"
 #include "SimulationRegion.h"
+#include "region.h"
+#include "neighbor_list.h"
 
 #define EPSILON (std::is_same<VALUETYPE, double>::value ? 1e-10 : 1e-4)
 
