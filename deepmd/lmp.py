bc9405398318083fdfb13c9aa2b350681e5c89eb
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-12-18 01:35:33 -0500

    fix loading library on macos
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd/lmp.py
+++ deepmd/lmp.py
@@ -1,13 +1,21 @@
 """Register entry points for lammps-wheel."""
 import os
+import platform
 from pathlib import Path
 
 from deepmd.env import tf
 
+if platform.system() == "Linux":
+    lib_env = "LD_LIBRARY_PATH"
+elif platform.system() == "Darwin":
+    lib_env = "DYLD_FALLBACK_LIBRARY_PATH"
+else:
+    raise RuntimeError("Unsupported platform")
+
 tf_dir = tf.sysconfig.get_lib()
 # set LD_LIBRARY_PATH
-os.environ["LD_LIBRARY_PATH"] = ":".join((
-    os.environ.get("LD_LIBRARY_PATH", ""),
+os.environ[lib_env] = ":".join((
+    os.environ.get(lib_env, ""),
     tf_dir,
     os.path.join(tf_dir, "python"),
 ))
