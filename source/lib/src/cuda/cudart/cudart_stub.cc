1d277570e3fd5d6278c2f6907eac47a441ecb913
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-12-27 22:01:33 -0500

    support CUDA 12.0
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/lib/src/cuda/cudart/cudart_stub.cc
+++ source/lib/src/cuda/cudart/cudart_stub.cc
@@ -75,8 +75,12 @@ cudaError_t GetSymbolNotFoundError() {
 #include "cuda_runtime_10_2.inc"
 #elif CUDART_VERSION < 11020
 #include "cuda_runtime_11_0.inc"
-#else
+#elif CUDART_VERSION < 11080
 #include "cuda_runtime_11_2.inc"
+#elif CUDART_VERSION < 12000
+#include "cuda_runtime_11_8.inc"
+#else
+#include "cuda_runtime_12_0.inc"
 #endif
 #undef __dv
 #undef __CUDA_DEPRECATED
