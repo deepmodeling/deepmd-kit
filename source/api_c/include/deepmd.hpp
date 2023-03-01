5fdb81a453e23229191ec5d2ae764626f22352fa
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-02-28 19:12:14 -0500

    fix: add declaration of strlen to deepmd.hpp
    Fix `error: ‘strlen’ was not declared in this scope`
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/api_c/include/deepmd.hpp
+++ source/api_c/include/deepmd.hpp
@@ -11,6 +11,7 @@ This header-only library provides a C++ 11 interface to the DeePMD-kit C API.
 #include <exception>
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 
 #include "c_api.h"
@@ -34,7 +35,7 @@ struct deepmd_exception : public std::runtime_error {
  */
 #define DP_CHECK_OK(check_func, dp)     \
   const char *err_msg = check_func(dp); \
-  if (strlen(err_msg))                  \
+  if (std::strlen(err_msg))                  \
     throw deepmd::hpp::deepmd_exception(std::string(err_msg));
 
 template <typename FPTYPE>
