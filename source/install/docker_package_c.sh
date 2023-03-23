cdb85fe12e585a1c09dd2044aa0b90526b489dd3
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-03-23 16:23:59 -0400

    bugfix
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/install/docker_package_c.sh
+++ source/install/docker_package_c.sh
@@ -6,6 +6,6 @@ docker run --rm -v ${SCRIPT_PATH}/../..:/root/deepmd-kit -w /root/deepmd-kit \
 	tensorflow/build:2.13-python3.11 \
 	/bin/sh -c "pip install tensorflow cmake \
             && cd /root/deepmd-kit/source/install \
-            && export CC = /dt9/usr/bin/gcc \
-            && export CXX = /dt9/usr/bin/g++ \
-            && /bin/sh package_c.sh"
+            && CC = /dt9/usr/bin/gcc \
+               CXX = /dt9/usr/bin/g++ \
+               /bin/sh package_c.sh"
