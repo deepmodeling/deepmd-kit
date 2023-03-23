130f4a433bc1f0515095828997f7eaae8f48f38e
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-03-23 16:40:20 -0400

    do not use containers for cc
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/install/docker_package_c.sh
+++ source/install/docker_package_c.sh
@@ -6,6 +6,6 @@ docker run --rm -v ${SCRIPT_PATH}/../..:/root/deepmd-kit -w /root/deepmd-kit \
 	tensorflow/build:2.13-python3.11 \
 	/bin/sh -c "pip install tensorflow cmake \
             && cd /root/deepmd-kit/source/install \
-            && CC = /dt9/usr/bin/gcc \
-               CXX = /dt9/usr/bin/g++ \
+            && CC=/dt9/usr/bin/gcc \
+               CXX=/dt9/usr/bin/g++ \
                /bin/sh package_c.sh"
