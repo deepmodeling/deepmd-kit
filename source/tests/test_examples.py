f40fc494dd7ddf625f45383bbeaf187d2dc6cb31
hztttt <940755193@qq.com>
2023-05-03 14:01:14 +0800

    add spin to test_examples

--- source/tests/test_examples.py
+++ source/tests/test_examples.py
@@ -32,6 +32,7 @@ input_files = (
     p_examples / "fparam" / "train" / "input.json",
     p_examples / "fparam" / "train" / "input_aparam.json",
     p_examples / "dos" / "train" / "input.json",
+    p_examples / "spin" / "se_e2_a" / "input.json",
 )
 
 
