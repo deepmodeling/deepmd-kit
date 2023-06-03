4818fbf62639265dfa131af9e4d25361a375f081
Yifan Li李一帆 <yifanl0716@gmail.com>
2023-06-03 03:05:37 -0400

    revert the changes in get_valid_pairs

--- source/lmp/fix_dplr.cpp
+++ source/lmp/fix_dplr.cpp
@@ -215,13 +215,9 @@ void FixDPLR::get_valid_pairs(vector<pair<int, int> > &pairs) {
                  "find a bonded pair the types of which are not associated");
     }
     if (!(idx0 < nlocal && idx1 < nlocal)) {
-      int idx0_ = atom->map(atom->tag[idx0]);
-      int idx1_ = atom->map(atom->tag[idx1]);
-      if (!(idx0_ >= 0 && idx0_ < nlocal && idx1_ >= 0 && idx1_ < nlocal)) {
-        error->all(FLERR,
-                   "find a bonded pair that is not on the same processor, "
-                   "something should not happen");
-      }
+      error->all(FLERR,
+                 "find a bonded pair that is not on the same processor, "
+                 "something should not happen");
     }
     pairs.push_back(pair<int, int>(idx0, idx1));
   }
