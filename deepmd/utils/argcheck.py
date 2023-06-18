a54c2f46c223cd7d6df45ba7e9f2123808e58267
nahso <47053538+nahso@users.noreply.github.com>
2023-06-18 16:28:57 +0800

    Update deepmd/utils/argcheck.py
    Co-authored-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
    Signed-off-by: nahso <47053538+nahso@users.noreply.github.com>

--- deepmd/utils/argcheck.py
+++ deepmd/utils/argcheck.py
@@ -334,7 +334,7 @@ def descrpt_hybrid_args():
 
 @descrpt_args_plugin.register("se_atten")
 def descrpt_se_atten_args():
-    doc_stripped_type_embedding = "Whether to strip the type embedding into a seperate embedding net. Setting it to `False` will fallback to the previous version of `se_atten` which is non-compressible. "
+    doc_stripped_type_embedding = "Whether to strip the type embedding into a separated embedding network. Setting it to `False` will fall back to the previous version of `se_atten` which is non-compressible."
     doc_sel = 'This parameter set the number of selected neighbors. Note that this parameter is a little different from that in other descriptors. Instead of separating each type of atoms, only the summation matters. And this number is highly related with the efficiency, thus one should not make it too large. Usually 200 or less is enough, far away from the GPU limitation 4096. It can be:\n\n\
     - `int`. The maximum number of neighbor atoms to be considered. We recommend it to be less than 200. \n\n\
     - `List[int]`. The length of the list should be the same as the number of atom types in the system. `sel[i]` gives the selected number of type-i neighbors. Only the summation of `sel[i]` matters, and it is recommended to be less than 200.\
