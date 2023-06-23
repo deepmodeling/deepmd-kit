837614fa3b47bca269f4f2f62b3cb337b9e07c1b
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-06-23 19:09:53 -0400

    Update se_atten.py
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd/descriptor/se_atten.py
+++ deepmd/descriptor/se_atten.py
@@ -649,6 +649,7 @@ class DescrptSeAtten(DescrptSeA):
             type_i,
             natoms,
             name="filter_type_all" + suffix,
+            suffix=suffix
             reuse=reuse,
             trainable=trainable,
             activation_fn=self.filter_activation_fn,
@@ -1015,7 +1016,7 @@ class DescrptSeAtten(DescrptSeA):
                         self.filter_precision,
                         activation_fn=activation_fn,
                         resnet_dt=self.filter_resnet_dt,
-                        name_suffix=suffix,
+                        name_suffix="",
                         stddev=stddev,
                         bavg=bavg,
                         seed=self.seed,
@@ -1040,7 +1041,7 @@ class DescrptSeAtten(DescrptSeA):
                             self.filter_precision,
                             activation_fn=activation_fn,
                             resnet_dt=self.filter_resnet_dt,
-                            name_suffix=suffix,
+                            name_suffix="",
                             stddev=stddev,
                             bavg=bavg,
                             seed=self.seed,
@@ -1089,7 +1090,7 @@ class DescrptSeAtten(DescrptSeA):
                             two_side_type_embedding,
                             [-1, two_side_type_embedding.shape[-1]],
                         )
-                        two_side_type_embedding_suffix = suffix + "_two_side_ebd"
+                        two_side_type_embedding_suffix = "_two_side_ebd"
                         embedding_of_two_side_type_embedding = embedding_net(
                             two_side_type_embedding,
                             self.filter_neuron,
