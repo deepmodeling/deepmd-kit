923333d537888327f2e8d8e58606cce74bf8ec60
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-08-02 16:08:50 -0400

    raise error if both v1 and v2 parameters are given
    Fix #2656.
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd/utils/compat.py
+++ deepmd/utils/compat.py
@@ -303,6 +303,8 @@ def convert_input_v1_v2(
     tr_data_cfg = {k: v for k, v in tr_cfg.items() if k in tr_data_keys}
     new_tr_cfg = {k: v for k, v in tr_cfg.items() if k not in tr_data_keys}
     new_tr_cfg["training_data"] = tr_data_cfg
+    if "training_data" in tr_cfg:
+        raise RuntimeError("Both v1 and v2 parameters are given.")
 
     jdata["training"] = new_tr_cfg
 
