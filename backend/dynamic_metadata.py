3f167ebf0def261353ac6da0ae85d4ed4592cee5
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-09-07 17:03:03 -0400

    fix compatible with Python 3.7
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- backend/dynamic_metadata.py
+++ backend/dynamic_metadata.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 from typing import (
     Dict,
+    List,
 )
 
 from .find_tensorflow import (
@@ -13,7 +14,7 @@ from .read_env import (
 __all__ = ["dynamic_metadata"]
 
 
-def __dir__() -> list[str]:
+def __dir__() -> List[str]:
     return __all__
 
 
