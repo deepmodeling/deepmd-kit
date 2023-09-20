338018ceffe9017f6225c05c9d3e04011b46b0d1
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-09-20 03:49:03 -0400

    merge CUDA and ROCm in header files (#2845)
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/lib/include/gelu.h
+++ source/lib/include/gelu.h
@@ -20,7 +20,7 @@ void gelu_grad_grad_cpu(FPTYPE* out,
                         const FPTYPE* dy_2,
                         const int_64 size);
 
-#if GOOGLE_CUDA
+#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 template <typename FPTYPE>
 void gelu_gpu(FPTYPE* out, const FPTYPE* xx, const int_64 size);
 
@@ -36,24 +36,5 @@ void gelu_grad_grad_gpu(FPTYPE* out,
                         const FPTYPE* dy,
                         const FPTYPE* dy_2,
                         const int_64 size);
-#endif  // GOOGLE_CUDA
-
-#if TENSORFLOW_USE_ROCM
-template <typename FPTYPE>
-void gelu_gpu(FPTYPE* out, const FPTYPE* xx, const int_64 size);
-
-template <typename FPTYPE>
-void gelu_grad_gpu(FPTYPE* out,
-                   const FPTYPE* xx,
-                   const FPTYPE* dy,
-                   const int_64 size);
-
-template <typename FPTYPE>
-void gelu_grad_grad_gpu(FPTYPE* out,
-                        const FPTYPE* xx,
-                        const FPTYPE* dy,
-                        const FPTYPE* dy_2,
-                        const int_64 size);
-
-#endif  // TENSORFLOW_USE_ROCM
+#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 }  // namespace deepmd
