ce92bac18b25a06c06c281555d59beccd1c0db82
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-10-03 22:36:33 -0400

    make neighbor atoms in parallel
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/lib/src/gpu/neighbor_stat.cu
+++ source/lib/src/gpu/neighbor_stat.cu
@@ -12,6 +12,9 @@ namespace cub = hipcub;
 #include "device.h"
 #include "neighbor_list.h"
 
+__device__ inline double _sqrt(double x) { return sqrt(x); }
+__device__ inline float _sqrt(float x) { return sqrtf(x); }
+
 template <typename FPTYPE>
 __global__ void neighbor_stat_g(const FPTYPE* coord,
                                 const int* type,
@@ -23,35 +26,34 @@ __global__ void neighbor_stat_g(const FPTYPE* coord,
                                 FPTYPE* min_nbor_dist,
                                 const int ntypes,
                                 const int MAX_NNEI) {
-  int ii = blockIdx.x * blockDim.x + threadIdx.x;
+  int ithread = blockIdx.x * blockDim.x + threadIdx.x;
+  int ii = ithread / MAX_NNEI;
+  int jj = ithread % MAX_NNEI;
   if (ii >= nloc) {
     return;
   }
   int idx_i = ilist[ii];
   if (type[idx_i] < 0) {
     // set all to 10000
-    for (int jj = 0; jj < MAX_NNEI; jj++) {
-      min_nbor_dist[ii * MAX_NNEI + jj] = INFINITY;
-    }
+    min_nbor_dist[ii * MAX_NNEI + jj] = INFINITY;
     return;  // virtual atom
   }
-  for (int jj = 0; jj < numneigh[ii]; jj++) {
+  if (jj < numneigh[ii]) {
     int idx_j = firstneigh[ii][jj];
     int type_j = type[idx_j];
     if (type_j < 0) {
       min_nbor_dist[ii * MAX_NNEI + jj] = INFINITY;
-      continue;  // virtual atom
+      return;  // virtual atom
     }
-    ++max_nbor_size[ii * ntypes + type_j];
+    atomicAdd(max_nbor_size + ii * ntypes + type_j, 1);
     FPTYPE rij[3] = {coord[idx_j * 3 + 0] - coord[idx_i * 3 + 0],
                      coord[idx_j * 3 + 1] - coord[idx_i * 3 + 1],
                      coord[idx_j * 3 + 2] - coord[idx_i * 3 + 2]};
     // we do not need to use the real index
     min_nbor_dist[ii * MAX_NNEI + jj] =
-        sqrt(rij[0] * rij[0] + rij[1] * rij[1] + rij[2] * rij[2]);
-  }
-  // set others to 10000
-  for (int jj = numneigh[ii]; jj < MAX_NNEI; ++jj) {
+        _sqrt(rij[0] * rij[0] + rij[1] * rij[1] + rij[2] * rij[2]);
+  } else {
+    // set others to 10000
     min_nbor_dist[ii * MAX_NNEI + jj] = INFINITY;
   }
 }
@@ -71,7 +73,7 @@ void neighbor_stat_gpu(const FPTYPE* coord,
   DPErrcheck(gpuDeviceSynchronize());
 
   DPErrcheck(gpuMemset(max_nbor_size, 0, sizeof(int) * int_64(nloc) * ntypes));
-  const int nblock_loc = (nloc + TPB - 1) / TPB;
+  const int nblock_loc = (nloc * MAX_NNEI + TPB - 1) / TPB;
   neighbor_stat_g<<<nblock_loc, TPB>>>(
       coord, type, nloc, gpu_nlist.ilist, gpu_nlist.firstneigh,
       gpu_nlist.numneigh, max_nbor_size, min_nbor_dist, ntypes, MAX_NNEI);
