b426d14f0fe362f04d22b07a0cad0f24c1669b38
Han Wang <92130845+wanghan-iapcm@users.noreply.github.com>
2023-10-11 10:35:00 +0800

    fix: make the se attn v2 descriptor energy conservative.  (#2905)
    This PR fixes issue #2811
    1. fix the auto-diff issue: input should be `descriptor_reshape`. 
    2. fix the discontinuity introduced in the attention map.
    
    ---------
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
    Co-authored-by: Han Wang <wang_han@iapcm.ac.cn>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd/op/_tabulate_grad.py
+++ deepmd/op/_tabulate_grad.py
@@ -55,7 +55,7 @@ def _tabulate_fusion_se_atten_grad_cc(op, dy):
         op.outputs[0],
         is_sorted=op.get_attr("is_sorted"),
     )
-    return [None, None, dy_dx, dy_df, None]
+    return [None, None, dy_dx, dy_df, dy_dtwo]
 
 
 @ops.RegisterGradient("TabulateFusionSeAttenGrad")
@@ -68,6 +68,7 @@ def _tabulate_fusion_se_atten_grad_grad_cc(op, dy, dy_, dy_dtwo):
         op.inputs[4],
         dy,
         dy_,
+        dy_dtwo,
         op.inputs[6],
         is_sorted=op.get_attr("is_sorted"),
     )
