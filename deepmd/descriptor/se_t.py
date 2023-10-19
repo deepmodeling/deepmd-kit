299184c71267074ebf00c019621c712ac2ed6c51
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-10-18 23:05:25 -0400

    support compressing any neuron structure (#2933)
    Fix #1370.
    
    Support compressing any neuron structure; fix compression error when
    `neuron[0]` is 1 (although no one will set `neuron[0]` to 1).
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd/descriptor/se_t.py
+++ deepmd/descriptor/se_t.py
@@ -349,14 +349,6 @@ class DescrptSeT(DescrptSe):
             not self.filter_resnet_dt
         ), "Model compression error: descriptor resnet_dt must be false!"
 
-        for ii in range(len(self.filter_neuron) - 1):
-            if self.filter_neuron[ii] * 2 != self.filter_neuron[ii + 1]:
-                raise NotImplementedError(
-                    "Model Compression error: descriptor neuron [%s] is not supported by model compression! "
-                    "The size of the next layer of the neural network must be twice the size of the previous layer."
-                    % ",".join([str(item) for item in self.filter_neuron])
-                )
-
         self.compress = True
         self.table = DPTabulate(
             self,
