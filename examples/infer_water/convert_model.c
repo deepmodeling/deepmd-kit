bee9983637a0a90b717f4f5e7dc215bd64a3b46e
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-10-25 23:44:25 -0400

    fix model path

--- examples/infer_water/convert_model.c
+++ examples/infer_water/convert_model.c
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "deepmd/c_api.h"
 
 int main() {
-  DP_ConvertPbtxtToPb("../../../source/tests/infer/deeppot.pbtxt", "graph.pb");
+  DP_ConvertPbtxtToPb("../../source/tests/infer/deeppot.pbtxt", "graph.pb");
   return 0;
 }
