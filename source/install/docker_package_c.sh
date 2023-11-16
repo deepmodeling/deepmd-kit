55d678dcf34f78ab2f1eacbc39d1f4ae78b9c316
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-11-16 00:30:55 -0500

    bump CUDA version to 12.2 for pre-built packages (#2960)
    TensorFlow 2.15 bumps the CUDA version to 12.2. See
    https://github.com/tensorflow/tensorflow/commit/3de44168950a5972ba4cfa7e3c6cbf4cffa67fe6.
    
    ---------
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/install/docker_package_c.sh
+++ source/install/docker_package_c.sh
@@ -3,7 +3,7 @@ set -e
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 
 docker run --rm -v ${SCRIPT_PATH}/../..:/root/deepmd-kit -w /root/deepmd-kit \
-	tensorflow/build:2.13-python3.11 \
+	tensorflow/build:2.15-python3.11 \
 	/bin/sh -c "pip install tensorflow cmake \
             && cd /root/deepmd-kit/source/install \
             && CC=/dt9/usr/bin/gcc \
