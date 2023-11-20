a87a0cb707d83bb797fc1270339764e841f8dd7e
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-11-20 18:05:31 -0500

    build cu11 for docker and C interface library

--- source/install/docker_package_c.sh
+++ source/install/docker_package_c.sh
@@ -3,8 +3,8 @@ set -e
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 
 docker run --rm -v ${SCRIPT_PATH}/../..:/root/deepmd-kit -w /root/deepmd-kit \
-	tensorflow/build:2.15-python3.11 \
-	/bin/sh -c "pip install tensorflow cmake \
+	tensorflow/build:${TENSORFLOW_BUILD_VERSION:-2.15}-python3.11 \
+	/bin/sh -c "pip install tensorflow${TENSORFLOW_VERSION} cmake \
             && cd /root/deepmd-kit/source/install \
             && CC=/dt9/usr/bin/gcc \
                CXX=/dt9/usr/bin/g++ \
