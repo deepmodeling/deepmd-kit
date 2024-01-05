ee7a15466a765eddf387c42c8adffa76b04e1e81
Han Wang <wang_han@iapcm.ac.cn>
2024-01-05 19:12:10 +0800

    test more input shapes

--- source/tests/test_model_format_utils.py
+++ source/tests/test_model_format_utils.py
@@ -18,19 +18,23 @@ from deepmd_utils.model_format import (
 
 class TestNativeLayer(unittest.TestCase):
     def test_serialize_deserize(self):
-        for (ni, no), bias, ut, activation_function, resnet in itertools.product(
+        for (ni, no), bias, ut, activation_function, resnet, ashp in itertools.product(
             [(5, 5), (5, 10), (5, 9), (9, 5)],
             [True, False],
             [True, False],
             ["tanh", "none"],
             [True, False],
+            [None, [4], [3, 2]],
         ):
             ww = np.full((ni, no), 3.0)
             bb = np.full((no,), 4.0) if bias else None
             idt = np.full((no,), 5.0) if ut else None
             nl0 = NativeLayer(ww, bb, idt, activation_function, resnet)
             nl1 = NativeLayer.deserialize(nl0.serialize())
-            inp = np.arange(ww.shape[0])
+            inp_shap = [ww.shape[0]]
+            if ashp is not None:
+              inp_shap = ashp + inp_shap              
+            inp = np.arange(np.prod(inp_shap)).reshape(inp_shap)
             np.testing.assert_allclose(nl0.call(inp), nl1.call(inp))
 
 
