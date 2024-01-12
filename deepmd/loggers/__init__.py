be9e68007840ffac78d77e59e40d160b798ef330
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-01-12 16:57:13 -0500

    fix
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd/loggers/__init__.py
+++ deepmd/loggers/__init__.py
@@ -1,3 +1,8 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 """Alias of deepmd_utils.loggers for backward compatibility."""
-from deepmd_utils.loggers import *  # noqa: F403
+
+from deepmd_utils.loggers.loggers import (
+    set_log_handles,
+)
+
+__all__ = ["set_log_handles"]
---
+++ deepmd/loggers/loggers.py
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+"""Alias of deepmd_utils.loggers.loggers for backward compatibility."""
+from deepmd_utils.loggers.loggers import *  # noqa: F403
