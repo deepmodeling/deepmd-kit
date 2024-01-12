8c36e45fc6b19add31e26d2fc00f8ed8a529c72a
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-01-12 17:39:23 -0500

    x
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd/utils/data_system.py
+++ deepmd/utils/data_system.py
@@ -2,8 +2,12 @@
 """Alias for backward compatibility."""
 from deepmd_utils.utils.data_system import (
     DeepmdDataSystem,
+    prob_sys_size_ext,
+    process_sys_probs,
 )
 
 __all__ = [
     "DeepmdDataSystem",
+    "process_sys_probs",
+    "prob_sys_size_ext",
 ]
