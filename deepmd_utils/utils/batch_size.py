efe0962f5e48749d5dae6b0846046d8820ca1e36
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-01-20 09:11:59 -0500

    move `OutOfMemoryError` from `deepmd` to `deepmd_utils` (#3153)
    To prevent circular import
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd_utils/utils/batch_size.py
+++ deepmd_utils/utils/batch_size.py
@@ -12,7 +12,7 @@ from typing import (
 
 import numpy as np
 
-from deepmd.utils.errors import (
+from deepmd_utils.utils.errors import (
     OutOfMemoryError,
 )
 
---
+++ deepmd_utils/utils/errors.py
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+class OutOfMemoryError(Exception):
+    """This error is caused by out-of-memory (OOM)."""
