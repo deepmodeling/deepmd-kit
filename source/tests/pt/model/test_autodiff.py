98f9eb2fae6a93965e3ed64a6895ebd577c81049
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2024-02-06 22:53:28 +0800

    fix: remove autodiff UTs, no force and virial

--- source/tests/pt/model/test_autodiff.py
+++ source/tests/pt/model/test_autodiff.py
@@ -20,7 +20,6 @@ from .test_permutation import (
     model_dpa1,
     model_dpa2,
     model_se_e2_a,
-    model_zbl,
 )
 
 
@@ -192,18 +191,3 @@ class TestEnergyModelDPAUniVirial(unittest.TestCase, VirialTest):
         self.type_split = True
         self.model = get_model(model_params, sampled).to(env.DEVICE)
 
-
-class TestEnergyModelZBLForce(unittest.TestCase, ForceTest):
-    def setUp(self):
-        model_params = copy.deepcopy(model_zbl)
-        sampled = make_sample(model_params)
-        self.type_split = False
-        self.model = get_model(model_params, sampled).to(env.DEVICE)
-
-
-# class TestEnergyModelZBLVirial(unittest.TestCase, VirialTest):
-#     def setUp(self):
-#         model_params = copy.deepcopy(model_zbl)
-#         sampled = make_sample(model_params)
-#         self.type_split = False
-#         self.model = get_zbl_model(model_params, sampled).to(env.DEVICE)
