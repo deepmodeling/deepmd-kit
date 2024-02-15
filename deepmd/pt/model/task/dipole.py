06fb5bf021add677a1d7073c59c66cf4d539e5ba
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2024-02-15 18:02:09 +0800

    chore: restore dipole, split PR

--- deepmd/pt/model/task/dipole.py
+++ deepmd/pt/model/task/dipole.py
@@ -1,47 +1,21 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 import logging
-from typing import (
-    List,
-    Optional,
-)
 
 import torch
 
-from deepmd.pt.model.network.mlp import (
-    FittingNet,
-    NetworkCollection,
+from deepmd.pt.model.network.network import (
+    ResidualDeep,
 )
 from deepmd.pt.model.task.fitting import (
-    GeneralFitting,
+    Fitting,
 )
-from deepmd.pt.utils import (
-    env,
-)
-from deepmd.pt.utils.env import (
-    DEFAULT_PRECISION,
-)
-
-dtype = env.GLOBAL_PT_FLOAT_PRECISION
-device = env.DEVICE
 
 log = logging.getLogger(__name__)
 
 
-class DipoleFittingNet(GeneralFitting):
+class DipoleFittingNet(Fitting):
     def __init__(
-        self,
-        var_name: str,
-        ntypes: int,
-        dim_descrpt: int,
-        dim_out: int,
-        neuron: List[int] = [128, 128, 128],
-        resnet_dt: bool = True,
-        numb_fparam: int = 0,
-        numb_aparam: int = 0,
-        activation_function: str = "tanh",
-        precision: str = DEFAULT_PRECISION,
-        distinguish_types: bool = False,
-        **kwargs,
+        self, ntypes, embedding_width, neuron, out_dim, resnet_dt=True, **kwargs
     ):
         """Construct a fitting net for dipole.
 
@@ -52,32 +26,23 @@ class DipoleFittingNet(GeneralFitting):
         - bias_atom_e: Average enery per atom for each element.
         - resnet_dt: Using time-step in the ResNet construction.
         """
-        super().__init__(
-            var_name=var_name,
-            ntypes=ntypes,
-            dim_descrpt=dim_descrpt,
-            dim_out=dim_out,
-            neuron=neuron,
-            resnet_dt=resnet_dt,
-            numb_fparam=numb_fparam,
-            numb_aparam=numb_aparam,
-            activation_function=activation_function,
-            precision=precision,
-            distinguish_types=distinguish_types,
-            **kwargs,
-        )
-        self.old_impl = False  # this only supports the new implementation.
+        super().__init__()
+        self.ntypes = ntypes
+        self.embedding_width = embedding_width
+        self.out_dim = out_dim
 
-    def forward(
-        self,
-        descriptor: torch.Tensor,
-        atype: torch.Tensor,
-        gr: Optional[torch.Tensor] = None,
-        g2: Optional[torch.Tensor] = None,
-        h2: Optional[torch.Tensor] = None,
-        fparam: Optional[torch.Tensor] = None,
-        aparam: Optional[torch.Tensor] = None,
-    ):
+        filter_layers = []
+        one = ResidualDeep(
+            0, embedding_width, neuron, 0.0, out_dim=self.out_dim, resnet_dt=resnet_dt
+        )
+        filter_layers.append(one)
+        self.filter_layers = torch.nn.ModuleList(filter_layers)
+
+        if "seed" in kwargs:
+            log.info("Set seed to %d in fitting net.", kwargs["seed"])
+            torch.manual_seed(kwargs["seed"])
+
+    def forward(self, inputs, atype, atype_tebd, rot_mat):
         """Based on embedding net output, alculate total energy.
 
         Args:
@@ -90,35 +55,13 @@ class DipoleFittingNet(GeneralFitting):
         -------
         - vec_out: output vector. Its shape is [nframes, nloc, 3].
         """
-        in_dim = self.dim_descrpt + self.numb_fparam + self.numb_aparam
-
-        nframes, nloc, _ = descriptor.shape
-        gr = gr.view(nframes, nloc, -1, 3)
-        out_dim = gr.shape[2]  # m1
-        self.filter_layers = NetworkCollection(
-            1 if self.distinguish_types else 0,
-            self.ntypes,
-            network_type="fitting_network",
-            networks=[
-                FittingNet(
-                    in_dim,
-                    out_dim,
-                    self.neuron,
-                    self.activation_function,
-                    self.resnet_dt,
-                    self.precision,
-                    bias_out=True,
-                )
-                for ii in range(self.ntypes if self.distinguish_types else 1)
-            ],
-        )
-        # (nframes, nloc, m1)
-        out = self._forward_common(descriptor, atype, gr, g2, h2, fparam, aparam)[
-            self.var_name
-        ]
-        # (nframes * nloc, 1, m1)
-        out = out.view(-1, 1, out_dim)
-        gr = gr.view(nframes * nloc, -1, 3)
-        # (nframes, nloc, 3)
-        out = torch.bmm(out, gr).squeeze(-2).view(nframes, nloc, 3)
-        return {self.var_name: out.to(env.GLOBAL_PT_FLOAT_PRECISION)}
+        nframes, nloc, _ = inputs.size()
+        if atype_tebd is not None:
+            inputs = torch.concat([inputs, atype_tebd], dim=-1)
+        vec_out = self.filter_layers[0](inputs)  # Shape is [nframes, nloc, m1]
+        assert list(vec_out.size()) == [nframes, nloc, self.out_dim]
+        vec_out = vec_out.view(-1, 1, self.out_dim)
+        vec_out = (
+            torch.bmm(vec_out, rot_mat).squeeze(-2).view(nframes, nloc, 3)
+        )  # Shape is [nframes, nloc, 3]
+        return vec_out
