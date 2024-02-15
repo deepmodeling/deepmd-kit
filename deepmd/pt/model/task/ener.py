6ade44f1ff72804505f8ef56483dd029779eea69
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2024-02-15 11:07:38 +0800

    chore: refactor InvarFitting

--- deepmd/pt/model/task/ener.py
+++ deepmd/pt/model/task/ener.py
@@ -1,5 +1,4 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
-import copy
 import logging
 from typing import (
     List,
@@ -15,298 +14,31 @@ from deepmd.dpmodel import (
     OutputVariableDef,
     fitting_check_output,
 )
-from deepmd.pt.model.network.mlp import (
-    FittingNet,
-    NetworkCollection,
-)
+
 from deepmd.pt.model.network.network import (
     ResidualDeep,
 )
 from deepmd.pt.model.task.fitting import (
     Fitting,
+    GeneralFitting,
 )
 from deepmd.pt.utils import (
     env,
 )
 from deepmd.pt.utils.env import (
     DEFAULT_PRECISION,
-    PRECISION_DICT,
 )
 from deepmd.pt.utils.stat import (
     compute_output_bias,
 )
 from deepmd.pt.utils.utils import (
     to_numpy_array,
-    to_torch_tensor,
 )
 
 dtype = env.GLOBAL_PT_FLOAT_PRECISION
 device = env.DEVICE
 
 log = logging.getLogger(__name__)
-
-
-class GeneralFitting(Fitting):
-    def __init__(
-        self,
-        var_name: str,
-        ntypes: int,
-        dim_descrpt: int,
-        dim_out: int,
-        neuron: List[int] = [128, 128, 128],
-        resnet_dt: bool = True,
-        numb_fparam: int = 0,
-        numb_aparam: int = 0,
-        activation_function: str = "tanh",
-        precision: str = DEFAULT_PRECISION,
-        distinguish_types: bool = False,
-        **kwargs,
-    ):
-        """Construct a fitting net for energy.
-
-        Args:
-        - ntypes: Element count.
-        - embedding_width: Embedding width per atom.
-        - neuron: Number of neurons in each hidden layers of the fitting net.
-        - bias_atom_e: Average enery per atom for each element.
-        - resnet_dt: Using time-step in the ResNet construction.
-        """
-        super().__init__()
-        self.var_name = var_name
-        self.ntypes = ntypes
-        self.dim_descrpt = dim_descrpt
-        self.dim_out = dim_out
-        self.neuron = neuron
-        self.distinguish_types = distinguish_types
-        self.use_tebd = not self.distinguish_types
-        self.resnet_dt = resnet_dt
-        self.numb_fparam = numb_fparam
-        self.numb_aparam = numb_aparam
-        self.activation_function = activation_function
-        self.precision = precision
-        self.prec = PRECISION_DICT[self.precision]
-
-        # init constants
-        if self.numb_fparam > 0:
-            self.register_buffer(
-                "fparam_avg",
-                torch.zeros(self.numb_fparam, dtype=self.prec, device=device),
-            )
-            self.register_buffer(
-                "fparam_inv_std",
-                torch.ones(self.numb_fparam, dtype=self.prec, device=device),
-            )
-        else:
-            self.fparam_avg, self.fparam_inv_std = None, None
-        if self.numb_aparam > 0:
-            self.register_buffer(
-                "aparam_avg",
-                torch.zeros(self.numb_aparam, dtype=self.prec, device=device),
-            )
-            self.register_buffer(
-                "aparam_inv_std",
-                torch.ones(self.numb_aparam, dtype=self.prec, device=device),
-            )
-        else:
-            self.aparam_avg, self.aparam_inv_std = None, None
-
-        in_dim = self.dim_descrpt + self.numb_fparam + self.numb_aparam
-
-        self.old_impl = kwargs.get("old_impl", False)
-        if self.old_impl:
-            filter_layers = []
-            for type_i in range(self.ntypes):
-                bias_type = 0.0
-                one = ResidualDeep(
-                    type_i,
-                    self.dim_descrpt,
-                    self.neuron,
-                    bias_type,
-                    resnet_dt=self.resnet_dt,
-                )
-                filter_layers.append(one)
-            self.filter_layers_old = torch.nn.ModuleList(filter_layers)
-            self.filter_layers = None
-        else:
-            self.filter_layers = NetworkCollection(
-                1 if self.distinguish_types else 0,
-                self.ntypes,
-                network_type="fitting_network",
-                networks=[
-                    FittingNet(
-                        in_dim,
-                        self.dim_out,
-                        self.neuron,
-                        self.activation_function,
-                        self.resnet_dt,
-                        self.precision,
-                        bias_out=True,
-                    )
-                    for ii in range(self.ntypes if self.distinguish_types else 1)
-                ],
-            )
-            self.filter_layers_old = None
-
-        # very bad design...
-        if "seed" in kwargs:
-            log.info("Set seed to %d in fitting net.", kwargs["seed"])
-            torch.manual_seed(kwargs["seed"])
-
-    def serialize(self) -> dict:
-        """Serialize the fitting to dict."""
-        return {
-            "var_name": self.var_name,
-            "ntypes": self.ntypes,
-            "dim_descrpt": self.dim_descrpt,
-            "dim_out": self.dim_out,
-            "neuron": self.neuron,
-            "resnet_dt": self.resnet_dt,
-            "numb_fparam": self.numb_fparam,
-            "numb_aparam": self.numb_aparam,
-            "activation_function": self.activation_function,
-            "precision": self.precision,
-            "distinguish_types": self.distinguish_types,
-            "nets": self.filter_layers.serialize(),
-            "@variables": {
-                "fparam_avg": to_numpy_array(self.fparam_avg),
-                "fparam_inv_std": to_numpy_array(self.fparam_inv_std),
-                "aparam_avg": to_numpy_array(self.aparam_avg),
-                "aparam_inv_std": to_numpy_array(self.aparam_inv_std),
-            },
-            # "rcond": self.rcond ,
-            # "tot_ener_zero": self.tot_ener_zero ,
-            # "trainable": self.trainable ,
-            # "atom_ener": self.atom_ener ,
-            # "layer_name": self.layer_name ,
-            # "use_aparam_as_mask": self.use_aparam_as_mask ,
-            # "spin": self.spin ,
-            ## NOTICE:  not supported by far
-            "rcond": None,
-            "tot_ener_zero": False,
-            "trainable": True,
-            "atom_ener": None,
-            "layer_name": None,
-            "use_aparam_as_mask": False,
-            "spin": None,
-        }
-
-    @classmethod
-    def deserialize(cls, data: dict) -> "GeneralFitting":
-        data = copy.deepcopy(data)
-        variables = data.pop("@variables")
-        nets = data.pop("nets")
-        obj = cls(**data)
-        for kk in variables.keys():
-            obj[kk] = to_torch_tensor(variables[kk])
-        obj.filter_layers = NetworkCollection.deserialize(nets)
-        return obj
-
-    def _extend_f_avg_std(self, xx: torch.Tensor, nb: int) -> torch.Tensor:
-        return torch.tile(xx.view([1, self.numb_fparam]), [nb, 1])
-
-    def _extend_a_avg_std(self, xx: torch.Tensor, nb: int, nloc: int) -> torch.Tensor:
-        return torch.tile(xx.view([1, 1, self.numb_aparam]), [nb, nloc, 1])
-
-    def _forward_common(
-        self,
-        descriptor: torch.Tensor,
-        atype: torch.Tensor,
-        gr: Optional[torch.Tensor] = None,
-        g2: Optional[torch.Tensor] = None,
-        h2: Optional[torch.Tensor] = None,
-        fparam: Optional[torch.Tensor] = None,
-        aparam: Optional[torch.Tensor] = None,
-    ):
-        xx = descriptor
-        nf, nloc, nd = xx.shape
-        if hasattr(self, "bias_atom_e"):
-            self.bias_atom_e = self.bias_atom_e.view([self.ntypes, self.dim_out])
-
-        if nd != self.dim_descrpt:
-            raise ValueError(
-                "get an input descriptor of dim {nd},"
-                "which is not consistent with {self.dim_descrpt}."
-            )
-        # check fparam dim, concate to input descriptor
-        if self.numb_fparam > 0:
-            assert fparam is not None, "fparam should not be None"
-            assert self.fparam_avg is not None
-            assert self.fparam_inv_std is not None
-            if fparam.shape[-1] != self.numb_fparam:
-                raise ValueError(
-                    "get an input fparam of dim {fparam.shape[-1]}, ",
-                    "which is not consistent with {self.numb_fparam}.",
-                )
-            fparam = fparam.view([nf, self.numb_fparam])
-            nb, _ = fparam.shape
-            t_fparam_avg = self._extend_f_avg_std(self.fparam_avg, nb)
-            t_fparam_inv_std = self._extend_f_avg_std(self.fparam_inv_std, nb)
-            fparam = (fparam - t_fparam_avg) * t_fparam_inv_std
-            fparam = torch.tile(fparam.reshape([nf, 1, -1]), [1, nloc, 1])
-            xx = torch.cat(
-                [xx, fparam],
-                dim=-1,
-            )
-        # check aparam dim, concate to input descriptor
-        if self.numb_aparam > 0:
-            assert aparam is not None, "aparam should not be None"
-            assert self.aparam_avg is not None
-            assert self.aparam_inv_std is not None
-            if aparam.shape[-1] != self.numb_aparam:
-                raise ValueError(
-                    "get an input aparam of dim {aparam.shape[-1]}, ",
-                    "which is not consistent with {self.numb_aparam}.",
-                )
-            aparam = aparam.view([nf, nloc, self.numb_aparam])
-            nb, nloc, _ = aparam.shape
-            t_aparam_avg = self._extend_a_avg_std(self.aparam_avg, nb, nloc)
-            t_aparam_inv_std = self._extend_a_avg_std(self.aparam_inv_std, nb, nloc)
-            aparam = (aparam - t_aparam_avg) * t_aparam_inv_std
-            xx = torch.cat(
-                [xx, aparam],
-                dim=-1,
-            )
-
-        outs = torch.zeros(nf, nloc, self.dim_out)  # jit assertion
-        if self.old_impl:
-            outs = torch.zeros_like(atype).unsqueeze(-1)  # jit assertion
-            assert self.filter_layers_old is not None
-            if self.use_tebd:
-                atom_property = self.filter_layers_old[0](xx) + self.bias_atom_e[
-                    atype
-                ].unsqueeze(-1)
-                outs = outs + atom_property  # Shape is [nframes, natoms[0], 1]
-            else:
-                for type_i, filter_layer in enumerate(self.filter_layers_old):
-                    mask = atype == type_i
-                    atom_property = filter_layer(xx)
-                    atom_property = (
-                        atom_property + self.bias_atom_e[type_i]
-                        if hasattr(self, "bias_atom_e")
-                        else atom_property
-                    )
-                    atom_property = atom_property * mask.unsqueeze(-1)
-                    outs = outs + atom_property  # Shape is [nframes, natoms[0], 1]
-            return {self.var_name: outs.to(env.GLOBAL_PT_FLOAT_PRECISION)}
-        else:
-            if self.use_tebd:
-                atom_property = (
-                    self.filter_layers.networks[0](xx) + self.bias_atom_e[atype]
-                )
-                outs = outs + atom_property  # Shape is [nframes, natoms[0], 1]
-            else:
-                for type_i, ll in enumerate(self.filter_layers.networks):
-                    mask = (atype == type_i).unsqueeze(-1)
-                    mask = torch.tile(mask, (1, 1, self.dim_out))
-                    atom_property = ll(xx)
-                    atom_property = (
-                        atom_property + self.bias_atom_e[type_i]
-                        if hasattr(self, "bias_atom_e")
-                        else atom_property
-                    )
-                    atom_property = atom_property * mask
-                    outs = outs + atom_property  # Shape is [nframes, natoms[0], 1]
-            return {self.var_name: outs.to(env.GLOBAL_PT_FLOAT_PRECISION)}
 
 
 @fitting_check_output
@@ -357,19 +89,6 @@ class InvarFitting(GeneralFitting):
         if not self.use_tebd:
             assert self.ntypes == bias_atom_e.shape[0], "Element count mismatches!"
         self.register_buffer("bias_atom_e", bias_atom_e)
-
-    def output_def(self) -> FittingOutputDef:
-        return FittingOutputDef(
-            [
-                OutputVariableDef(
-                    self.var_name,
-                    [self.dim_out],
-                    reduciable=True,
-                    r_differentiable=True,
-                    c_differentiable=True,
-                ),
-            ]
-        )
 
     def __setitem__(self, key, value):
         if key in ["bias_atom_e"]:
