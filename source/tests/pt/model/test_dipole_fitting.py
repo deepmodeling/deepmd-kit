7592c75d0431004dfd8f0d244851d314fd3fc711
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2024-02-18 12:14:41 +0800

    feat: add UTs

--- source/tests/pt/model/test_dipole_fitting.py
+++ source/tests/pt/model/test_dipole_fitting.py
@@ -18,15 +18,20 @@ from deepmd.pt.utils import (
 from deepmd.pt.utils.utils import (
     to_numpy_array,
 )
+from deepmd.pt.utils.nlist import (
+    extend_input_and_build_neighbor_list
+)
 
 from .test_env_mat import (
     TestCaseSingleFrameWithNlist,
 )
-
+from scipy.stats import (
+            special_ortho_group,
+        )
 dtype = env.GLOBAL_PT_FLOAT_PRECISION
 
 
-class TestInvarFitting(unittest.TestCase, TestCaseSingleFrameWithNlist):
+class TestDipoleFitting(unittest.TestCase, TestCaseSingleFrameWithNlist):
     def setUp(self):
         TestCaseSingleFrameWithNlist.setUp(self)
 
@@ -114,3 +119,133 @@ class TestInvarFitting(unittest.TestCase, TestCaseSingleFrameWithNlist):
                 use_tebd=(not distinguish_types),
             ).to(env.DEVICE)
             torch.jit.script(ft0)
+
+class TestEquivalence(unittest.TestCase):
+    def setUp(self) -> None:
+        self.natoms = 5
+        self.rcut = 4
+        self.rcut_smth=0.5
+        self.sel=[46, 92, 4]
+        self.nf = 1
+        self.coord = 2 * torch.rand([self.natoms, 3], dtype=dtype).to(env.DEVICE)
+        self.shift = torch.tensor([4, 4, 4], dtype=dtype).to(env.DEVICE)
+        self.atype = torch.IntTensor([0, 0, 0, 1, 1]).to(env.DEVICE)
+        self.dd0 = DescrptSeA(self.rcut, self.rcut_smth, self.sel).to(env.DEVICE)   
+        self.cell = torch.rand([3, 3], dtype=dtype).to(env.DEVICE)
+        self.cell = (self.cell + self.cell.T) + 5.0 * torch.eye(3).to(env.DEVICE)
+        
+    def test_rot(self):
+        atype = self.atype.reshape(1,5)
+        rmat = torch.tensor(special_ortho_group.rvs(3), dtype=dtype).to(env.DEVICE)
+        coord_rot = torch.matmul(self.coord, rmat)
+        rng = np.random.default_rng()
+        for distinguish_types, nfp, nap in itertools.product(
+                [True, False],
+                [0, 3],
+                [0, 4],
+            ):
+                ft0 = DipoleFittingNet(
+                    "foo",
+                    3, #ntype
+                    self.dd0.dim_out,
+                    3,
+                    dim_rot_mat=100,
+                    numb_fparam=nfp,
+                    numb_aparam=nap,
+                    use_tebd=False,
+                ).to(env.DEVICE)
+                if nfp > 0:
+                    ifp = torch.tensor(
+                        rng.normal(size=(self.nf, nfp)), dtype=dtype, device=env.DEVICE
+                    )
+                else:
+                    ifp = None
+                if nap > 0:
+                    iap = torch.tensor(
+                        rng.normal(size=(self.nf, self.natoms, nap)),
+                        dtype=dtype,
+                        device=env.DEVICE,
+                    )
+                else:
+                    iap = None
+
+                res = []
+                for xyz in [self.coord, coord_rot]:
+                    extended_coord, extended_atype, mapping, nlist = extend_input_and_build_neighbor_list(xyz + self.shift, atype, self.rcut, self.sel, distinguish_types)
+
+                    rd0, gr0, _, _, _ = self.dd0(
+                        extended_coord, extended_atype, nlist,
+                    )
+
+                    ret0 = ft0(rd0, extended_atype, gr0, fparam=ifp, aparam=iap)
+                    res.append(ret0['foo'])
+
+                np.testing.assert_allclose(
+                        to_numpy_array(res[1]),
+                        to_numpy_array(torch.matmul(res[0], rmat))
+                    )
+
+    def test_permu(self):
+        
+        coord = torch.matmul(self.coord, self.cell)
+        ft0 = DipoleFittingNet(
+                    "foo",
+                    3, #ntype
+                    self.dd0.dim_out,
+                    3,
+                    dim_rot_mat=100,
+                    numb_fparam=0,
+                    numb_aparam=0,
+                    use_tebd=False,
+                ).to(env.DEVICE)
+        res = []
+        for idx_perm in [[0, 1, 2, 3, 4], [1, 0, 4, 3, 2]]:
+            atype = self.atype[idx_perm].reshape(1, 5)
+            extended_coord, extended_atype, mapping, nlist = extend_input_and_build_neighbor_list(coord[idx_perm], atype, self.rcut, self.sel, False)
+
+            rd0, gr0, _, _, _ = self.dd0(
+                extended_coord, extended_atype, nlist,
+            )
+
+            ret0 = ft0(rd0, extended_atype, gr0, fparam=0, aparam=0)
+            res.append(ret0['foo'])
+        
+        np.testing.assert_allclose(
+                to_numpy_array(res[0][:, idx_perm]),
+                to_numpy_array(res[1])
+            )
+
+    def test_trans(self):
+        atype = self.atype.reshape(1, 5)
+        coord_s = torch.matmul(
+            torch.remainder(torch.matmul(self.coord + self.shift, torch.linalg.inv(self.cell)), 1.0),
+            self.cell,
+        )
+        ft0 = DipoleFittingNet(
+                    "foo",
+                    3, #ntype
+                    self.dd0.dim_out,
+                    3,
+                    dim_rot_mat=100,
+                    numb_fparam=0,
+                    numb_aparam=0,
+                    use_tebd=False,
+                ).to(env.DEVICE)
+        res = []
+        for xyz in [self.coord, coord_s]:
+            extended_coord, extended_atype, mapping, nlist = extend_input_and_build_neighbor_list(xyz + self.shift, atype, self.rcut, self.sel, False)
+
+            rd0, gr0, _, _, _ = self.dd0(
+                extended_coord, extended_atype, nlist,
+            )
+            
+            ret0 = ft0(rd0, extended_atype, gr0, fparam=0, aparam=0)
+            res.append(ret0['foo'])
+
+        np.testing.assert_allclose(
+                to_numpy_array(res[0]),
+                to_numpy_array(res[1])
+            )
+
+if __name__ == "__main__":
+    unittest.main()
