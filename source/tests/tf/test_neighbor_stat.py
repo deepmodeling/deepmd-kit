deepmodeling/deepmd-kit

commit 9f6ff1e925ab00f2b0d40e549438009e3f3bb016

fix: replace all distinguished types by mixed types. (#3289)
Signed-off-by: Han Wang <92130845+wanghan-iapcm@users.noreply.github.com>
Co-authored-by: Han Wang <wang_han@iapcm.ac.cn>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

deepmd/dpmodel/utils/neighbor_stat.py
@@ -28,19 +28,19 @@ class NeighborStatOP(NativeOP):
         The num of atom types
     rcut
         The cut-off radius
-    distinguish_types : bool, optional
-        If False, treat all types as a single type.
+    mixed_types : bool, optional
+        If True, treat all types as a single type.
     """
 
     def __init__(
         self,
         ntypes: int,
         rcut: float,
-        distinguish_types: bool,
+        mixed_types: bool,
     ) -> None:
         self.rcut = rcut
         self.ntypes = ntypes
-        self.distinguish_types = distinguish_types
+        self.mixed_types = mixed_types
 
     def call(
         self,
@@ -89,7 +89,7 @@ class NeighborStatOP(NativeOP):
         rr2 = np.sum(np.square(diff), axis=-1)
         min_rr2 = np.min(rr2, axis=-1)
         # count the number of neighbors
-        if self.distinguish_types:
+        if not self.mixed_types:
             mask = rr2 < self.rcut**2
             nnei = np.zeros((nframes, nloc, self.ntypes), dtype=int)
             for ii in range(self.ntypes):

deepmd/pt/utils/neighbor_stat.py
@@ -32,20 +32,20 @@ class NeighborStatOP(torch.nn.Module):
         The num of atom types
     rcut
         The cut-off radius
-    distinguish_types : bool, optional
-        If False, treat all types as a single type.
+    mixed_types : bool, optional
+        If True, treat neighbors of all types as a single type.
     """
 
     def __init__(
         self,
         ntypes: int,
         rcut: float,
-        distinguish_types: bool,
+        mixed_types: bool,
     ) -> None:
         super().__init__()
         self.rcut = rcut
         self.ntypes = ntypes
-        self.distinguish_types = distinguish_types
+        self.mixed_types = mixed_types
 
     def forward(
         self,
@@ -94,7 +94,7 @@ class NeighborStatOP(torch.nn.Module):
         rr2 = torch.sum(torch.square(diff), dim=-1)
         min_rr2, _ = torch.min(rr2, dim=-1)
         # count the number of neighbors
-        if self.distinguish_types:
+        if not self.mixed_types:
             mask = rr2 < self.rcut**2
             nnei = torch.zeros((nframes, nloc, self.ntypes), dtype=torch.int32)
             for ii in range(self.ntypes):

deepmd/tf/utils/neighbor_stat.py
@@ -40,20 +40,20 @@ class NeighborStatOP:
         The num of atom types
     rcut
         The cut-off radius
-    distinguish_types : bool, optional
-        If False, treat all types as a single type.
+    mixed_types : bool, optional
+        If True, treat neighbors of all types as a single type.
     """
 
     def __init__(
         self,
         ntypes: int,
         rcut: float,
-        distinguish_types: bool,
+        mixed_types: bool,
     ) -> None:
         super().__init__()
         self.rcut = rcut
         self.ntypes = ntypes
-        self.distinguish_types = distinguish_types
+        self.mixed_types = mixed_types
 
     def build(
         self,
@@ -117,7 +117,7 @@ class NeighborStatOP:
         rr2 = tf.where(mask, inf_mask, rr2)
         min_rr2 = tf.reduce_min(rr2, axis=(1, 2))
         # count the number of neighbors
-        if self.distinguish_types:
+        if not self.mixed_types:
             mask = rr2 < self.rcut**2
             nnei = []
             for ii in range(self.ntypes):
@@ -148,7 +148,7 @@ class NeighborStatOP:
         nnei = tf.where(
             tf.tile(
                 tf.less(atype, 0)[:, :, None],
-                [1, 1, self.ntypes if self.distinguish_types else 1],
+                [1, 1, self.ntypes if not self.mixed_types else 1],
             ),
             tf.zeros_like(nnei, dtype=tf.int32),
             nnei,

deepmd/dpmodel/atomic_model/dp_atomic_model.py
@@ -66,11 +66,17 @@ class DPAtomicModel(BaseAtomicModel):
         """Get the type map."""
         return self.type_map
 
-    def distinguish_types(self) -> bool:
-        """Returns if model requires a neighbor list that distinguish different
-        atomic types or not.
+    def mixed_types(self) -> bool:
+        """If true, the model
+        1. assumes total number of atoms aligned across frames;
+        2. uses a neighbor list that does not distinguish different atomic types.
+
+        If false, the model
+        1. assumes total number of atoms of each atom type aligned across frames;
+        2. uses a neighbor list that distinguishes different atomic types.
+
         """
-        return self.descriptor.distinguish_types()
+        return self.descriptor.mixed_types()
 
     def forward_atomic(
         self,

deepmd/dpmodel/atomic_model/linear_atomic_model.py
@@ -50,13 +50,19 @@ class LinearAtomicModel(BaseAtomicModel):
     ):
         super().__init__()
         self.models = models
-        self.distinguish_type_list = [
-            model.distinguish_types() for model in self.models
-        ]
+        self.mixed_types_list = [model.mixed_types() for model in self.models]
 
-    def distinguish_types(self) -> bool:
-        """If distinguish different types by sorting."""
-        return False
+    def mixed_types(self) -> bool:
+        """If true, the model
+        1. assumes total number of atoms aligned across frames;
+        2. uses a neighbor list that does not distinguish different atomic types.
+
+        If false, the model
+        1. assumes total number of atoms of each atom type aligned across frames;
+        2. uses a neighbor list that distinguishes different atomic types.
+
+        """
+        return True
 
     def get_rcut(self) -> float:
         """Get the cut-off radius."""
@@ -134,9 +140,9 @@ class LinearAtomicModel(BaseAtomicModel):
             for rcut, sel in zip(self.get_model_rcuts(), self.get_model_nsels())
         ]
         nlists_ = [
-            nl if not dt else nlist_distinguish_types(nl, extended_atype, sel)
-            for dt, nl, sel in zip(
-                self.distinguish_type_list, raw_nlists, self.get_model_sels()
+            nl if mt else nlist_distinguish_types(nl, extended_atype, sel)
+            for mt, nl, sel in zip(
+                self.mixed_types_list, raw_nlists, self.get_model_sels()
             )
         ]
         ener_list = [

deepmd/dpmodel/atomic_model/make_base_atomic_model.py
@@ -86,9 +86,15 @@ def make_base_atomic_model(
             """
 
         @abstractmethod
-        def distinguish_types(self) -> bool:
-            """Returns if the model requires a neighbor list that distinguish different
-            atomic types or not.
+        def mixed_types(self) -> bool:
+            """If true, the model
+            1. assumes total number of atoms aligned across frames;
+            2. uses a neighbor list that does not distinguish different atomic types.
+
+            If false, the model
+            1. assumes total number of atoms of each atom type aligned across frames;
+            2. uses a neighbor list that distinguishes different atomic types.
+
             """
             pass
 

deepmd/dpmodel/atomic_model/pairtab_atomic_model.py
@@ -91,9 +91,18 @@ class PairTabAtomicModel(BaseAtomicModel):
     def get_nsel(self) -> int:
         return self.sel
 
-    def distinguish_types(self) -> bool:
+    def mixed_types(self) -> bool:
+        """If true, the model
+        1. assumes total number of atoms aligned across frames;
+        2. uses a neighbor list that does not distinguish different atomic types.
+
+        If false, the model
+        1. assumes total number of atoms of each atom type aligned across frames;
+        2. uses a neighbor list that distinguishes different atomic types.
+
+        """
         # to match DPA1 and DPA2.
-        return False
+        return True
 
     def serialize(self) -> dict:
         return {"tab": self.tab.serialize(), "rcut": self.rcut, "sel": self.sel}

deepmd/dpmodel/descriptor/make_base_descriptor.py
@@ -67,7 +67,7 @@ def make_base_descriptor(
             pass
 
         @abstractmethod
-        def distinguish_types(self) -> bool:
+        def mixed_types(self) -> bool:
             """Returns if the descriptor requires a neighbor list that distinguish different
             atomic types or not.
             """

commit ff62cf9089488648e17e5158bd8709e9f399d14b

fix neighbor stat mixed_types input
It was broken by #3289, which replaced distinguished types by mixed types but didn't change the input.

Add tests for two types.

Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

deepmd/dpmodel/utils/neighbor_stat.py
@@ -126,7 +126,7 @@ class NeighborStat(BaseNeighborStat):
         one_type: bool = False,
     ) -> None:
         super().__init__(ntypes, rcut, one_type)
-        self.op = NeighborStatOP(ntypes, rcut, not one_type)
+        self.op = NeighborStatOP(ntypes, rcut, one_type)
 
     def iterator(
         self, data: DeepmdDataSystem

deepmd/pt/utils/neighbor_stat.py
@@ -89,14 +89,16 @@ class NeighborStatOP(torch.nn.Module):
         )
         assert list(diff.shape) == [nframes, nloc, nall, 3]
         # remove the diagonal elements
-        mask = torch.eye(nloc, nall, dtype=torch.bool)
+        mask = torch.eye(nloc, nall, dtype=torch.bool, device=diff.device)
         diff[:, mask] = torch.inf
         rr2 = torch.sum(torch.square(diff), dim=-1)
         min_rr2, _ = torch.min(rr2, dim=-1)
         # count the number of neighbors
         if not self.mixed_types:
             mask = rr2 < self.rcut**2
-            nnei = torch.zeros((nframes, nloc, self.ntypes), dtype=torch.int32)
+            nnei = torch.zeros(
+                (nframes, nloc, self.ntypes), dtype=torch.int32, device=mask.device
+            )
             for ii in range(self.ntypes):
                 nnei[:, :, ii] = torch.sum(
                     mask & extend_atype.eq(ii)[:, None, :], dim=-1
@@ -131,7 +133,7 @@ class NeighborStat(BaseNeighborStat):
         one_type: bool = False,
     ) -> None:
         super().__init__(ntypes, rcut, one_type)
-        op = NeighborStatOP(ntypes, rcut, not one_type)
+        op = NeighborStatOP(ntypes, rcut, one_type)
         self.op = torch.jit.script(op)
         self.auto_batch_size = AutoBatchSize()
 

deepmd/tf/utils/neighbor_stat.py
@@ -181,7 +181,7 @@ class NeighborStat(BaseNeighborStat):
         """Constructor."""
         super().__init__(ntypes, rcut, one_type)
         self.auto_batch_size = AutoBatchSize()
-        self.neighbor_stat = NeighborStatOP(ntypes, rcut, not one_type)
+        self.neighbor_stat = NeighborStatOP(ntypes, rcut, one_type)
         self.place_holders = {}
         with tf.Graph().as_default() as sub_graph:
             self.op = self.build()

source/tests/common/dpmodel/test_neighbor_stat.py
@@ -45,7 +45,7 @@ class TestNeighborStat(unittest.TestCase):
                     min_nbor_dist, max_nbor_size = neighbor_stat(
                         system="system_0",
                         rcut=rcut,
-                        type_map=["TYPE"],
+                        type_map=["TYPE", "NO_THIS_TYPE"],
                         one_type=one_type,
                         backend="numpy",
                     )
@@ -58,4 +58,7 @@ class TestNeighborStat(unittest.TestCase):
                         np.logical_and(distance > 0, distance <= rcut)
                     )
                     self.assertAlmostEqual(min_nbor_dist, 1.0, 6)
-                    self.assertEqual(max_nbor_size, [expected_neighbors])
+                    ret = [expected_neighbors]
+                    if not one_type:
+                        ret.append(0)
+                    np.testing.assert_array_equal(max_nbor_size, ret)

source/tests/pt/test_neighbor_stat.py
@@ -45,7 +45,7 @@ class TestNeighborStat(unittest.TestCase):
                     min_nbor_dist, max_nbor_size = neighbor_stat(
                         system="system_0",
                         rcut=rcut,
-                        type_map=["TYPE"],
+                        type_map=["TYPE", "NO_THIS_TYPE"],
                         one_type=one_type,
                         backend="pytorch",
                     )
@@ -58,4 +58,7 @@ class TestNeighborStat(unittest.TestCase):
                         np.logical_and(distance > 0, distance <= rcut)
                     )
                     self.assertAlmostEqual(min_nbor_dist, 1.0, 6)
-                    self.assertEqual(max_nbor_size, [expected_neighbors])
+                    ret = [expected_neighbors]
+                    if not one_type:
+                        ret.append(0)
+                    np.testing.assert_array_equal(max_nbor_size, ret)

source/tests/tf/test_neighbor_stat.py
@@ -41,18 +41,25 @@ class TestNeighborStat(unittest.TestCase):
         # set rcut to 0. will cause a core dumped
         # TODO: check what is wrong
         for rcut in (1.0, 2.0, 4.0):
-            with self.subTest():
-                rcut += 1e-3  # prevent numerical errors
-                min_nbor_dist, max_nbor_size = neighbor_stat(
-                    system="system_0", rcut=rcut, type_map=["TYPE"]
-                )
-                upper = np.ceil(rcut) + 1
-                X, Y, Z = np.mgrid[-upper:upper, -upper:upper, -upper:upper]
-                positions = np.vstack([X.ravel(), Y.ravel(), Z.ravel()]).T
-                # distance to (0,0,0)
-                distance = np.linalg.norm(positions, axis=1)
-                expected_neighbors = np.count_nonzero(
-                    np.logical_and(distance > 0, distance <= rcut)
-                )
-                self.assertAlmostEqual(min_nbor_dist, 1.0, 6)
-                self.assertEqual(max_nbor_size, [expected_neighbors])
+            for one_type in (True, False):
+                with self.subTest(rcut=rcut, one_type=one_type):
+                    rcut += 1e-3  # prevent numerical errors
+                    min_nbor_dist, max_nbor_size = neighbor_stat(
+                        system="system_0",
+                        rcut=rcut,
+                        type_map=["TYPE", "NO_THIS_TYPE"],
+                        one_type=one_type,
+                    )
+                    upper = np.ceil(rcut) + 1
+                    X, Y, Z = np.mgrid[-upper:upper, -upper:upper, -upper:upper]
+                    positions = np.vstack([X.ravel(), Y.ravel(), Z.ravel()]).T
+                    # distance to (0,0,0)
+                    distance = np.linalg.norm(positions, axis=1)
+                    expected_neighbors = np.count_nonzero(
+                        np.logical_and(distance > 0, distance <= rcut)
+                    )
+                    self.assertAlmostEqual(min_nbor_dist, 1.0, 6)
+                    ret = [expected_neighbors]
+                    if not one_type:
+                        ret.append(0)
+                    np.testing.assert_array_equal(max_nbor_size, ret)