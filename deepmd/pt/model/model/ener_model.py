1fa1afc5c0577501de7b9a581db4ac287bc28c67
Lysithea <ucaszxy@outlook.com>
2024-02-28 12:01:10 +0800

    del ener model precison

--- deepmd/pt/model/model/ener_model.py
+++ deepmd/pt/model/model/ener_model.py
@@ -30,9 +30,6 @@ class EnergyModel(DPModel):
         aparam: Optional[torch.Tensor] = None,
         do_atomic_virial: bool = False,
     ) -> Dict[str, torch.Tensor]:
-        coord = coord.to(env.GLOBAL_PT_FLOAT_PRECISION)
-        if box is not None:
-            box = box.to(env.GLOBAL_PT_FLOAT_PRECISION)
         model_ret = self.forward_common(
             coord,
             atype,
