499ed1743d68b4854f7b63a4b1536b7c4c35b23f
Lysithea <ucaszxy@outlook.com>
2024-02-28 10:05:25 +0800

    remove memset

--- source/api_cc/src/commonPT.cc
+++ source/api_cc/src/commonPT.cc
@@ -12,7 +12,7 @@ void NeighborListDataPT::copy_from_nlist(const InputNlist& inlist,
   max_num_neighbors = *max_element;
   unsigned long nlist_size = (unsigned long)inum * max_num_neighbors;
   jlist.resize(nlist_size);
-  memset(&jlist[0], -1, nlist_size * sizeof(int));
+  jlist.assign(nlist_size, -1);
   for (int ii = 0; ii < inum; ++ii) {
     int jnum = inlist.numneigh[ii];
     numneigh[ii] = inlist.numneigh[ii];
