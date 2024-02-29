66edca55617edde721a0fd6b32a45b133defa118
Han Wang <92130845+wanghan-iapcm@users.noreply.github.com>
2024-02-29 20:17:00 +0800

    Update deepmd/pt/train/training.py
    Signed-off-by: Han Wang <92130845+wanghan-iapcm@users.noreply.github.com>

--- deepmd/pt/train/training.py
+++ deepmd/pt/train/training.py
@@ -167,12 +167,7 @@ class Trainer:
                     validation_dataloader,
                     validation_data_buffered,
                 ) = get_dataloader_and_buffer(_validation_data, _training_params)
-                if _training_params.get("validation_data", None) is not None:
-                    valid_numb_batch = _training_params["validation_data"].get(
-                        "numb_btch", 1
-                    )
-                else:
-                    valid_numb_batch = 1
+                valid_numb_batch = _training_params["validation_data"].get("numb_btch", 1)
             else:
                 validation_dataloader = None
                 validation_data_buffered = None
