c31d3760f5095efd95f4df2ddf37d86eaa88fd66
Duo <50307526+iProzd@users.noreply.github.com>
2024-03-03 13:50:55 +0800

    Merge `change_energy_bias` and fix finetune (#3378)
    Signed-off-by: Duo <50307526+iProzd@users.noreply.github.com>
    Signed-off-by: Han Wang <92130845+wanghan-iapcm@users.noreply.github.com>
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Han Wang <92130845+wanghan-iapcm@users.noreply.github.com>
    Co-authored-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/tests/pt/test_training.py
+++ source/tests/pt/test_training.py
@@ -26,8 +26,17 @@ from .model.test_permutation import (
 
 class DPTrainTest:
     def test_dp_train(self):
+        # test training from scratch
         trainer = get_trainer(deepcopy(self.config))
         trainer.run()
+
+        # test fine-tuning
+        trainer_finetune = get_trainer(
+            deepcopy(self.config),
+            finetune_model=self.config["training"].get("save_ckpt", "model.ckpt")
+            + ".pt",
+        )
+        trainer_finetune.run()
         self.tearDown()
 
     def test_trainable(self):
