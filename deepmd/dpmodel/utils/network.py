7af5b89ef65e1ba333b139a3a2f8fad962dffbb7
Han Wang <wang_han@iapcm.ac.cn>
2024-03-13 16:55:15 +0800

    fix the bug of empty fitting net neuron.

--- deepmd/dpmodel/utils/network.py
+++ deepmd/dpmodel/utils/network.py
@@ -561,7 +561,8 @@ def make_fitting_network(T_EmbeddingNet, T_Network, T_NetworkLayer):
                 resnet_dt=resnet_dt,
                 precision=precision,
             )
-            i_in, i_ot = neuron[-1], out_dim
+            i_in = neuron[-1] if len(neuron) > 0 else in_dim
+            i_ot = out_dim
             self.layers.append(
                 T_NetworkLayer(
                     i_in,
