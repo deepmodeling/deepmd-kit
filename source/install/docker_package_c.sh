6f2cef5af01acf7767a7e1294328978601d64419
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-04-15 20:57:38 -0400

    use uv in docker

--- source/install/docker_package_c.sh
+++ source/install/docker_package_c.sh
@@ -4,7 +4,8 @@ SCRIPT_PATH=$(dirname $(realpath -s $0))
 
 docker run --rm -v ${SCRIPT_PATH}/../..:/root/deepmd-kit -w /root/deepmd-kit \
 	tensorflow/build:${TENSORFLOW_BUILD_VERSION:-2.15}-python3.11 \
-	/bin/sh -c "pip install \"tensorflow${TENSORFLOW_VERSION}\" cmake \
+	/bin/sh -c "pip install --system uv \
+            && uv pip install \"tensorflow${TENSORFLOW_VERSION}\" cmake \
             && git config --global --add safe.directory /root/deepmd-kit \
             && cd /root/deepmd-kit/source/install \
             && CC=/dt9/usr/bin/gcc \
