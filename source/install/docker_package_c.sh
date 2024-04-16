97bd51e6a182ca1350c0fb4aa251188b00b6d530
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-04-15 22:24:31 -0400

    revert docker_package_c.sh; not sure what's wrong

--- source/install/docker_package_c.sh
+++ source/install/docker_package_c.sh
@@ -4,8 +4,7 @@ SCRIPT_PATH=$(dirname $(realpath -s $0))
 
 docker run --rm -v ${SCRIPT_PATH}/../..:/root/deepmd-kit -w /root/deepmd-kit \
 	tensorflow/build:${TENSORFLOW_BUILD_VERSION:-2.15}-python3.11 \
-	/bin/sh -c "pip install uv \
-            && uv pip install --system \"tensorflow${TENSORFLOW_VERSION}\" cmake \
+	/bin/sh -c "pip install \"tensorflow${TENSORFLOW_VERSION}\" cmake \
             && git config --global --add safe.directory /root/deepmd-kit \
             && cd /root/deepmd-kit/source/install \
             && CC=/dt9/usr/bin/gcc \
