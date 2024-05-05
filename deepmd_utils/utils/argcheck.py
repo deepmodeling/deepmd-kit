ef73b7ab7600113d68654af2fd744389e75507e9
nahso <47053538+nahso@users.noreply.github.com>
2024-05-06 04:28:21 +0800

    Clarifiy se_atten_v2 compression doc (#3727)
    https://github.com/deepmodeling/deepmd-kit/issues/3643
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    - **Documentation**
    - Simplified the description for the number of attention layers in the
    code documentation.
    - Added a notice about model compression compatibility for `se_atten_v2`
    descriptor in the documentation.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
    Co-authored-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
    (cherry picked from commit 62832e85d3c370c2ce4c66c559ee88094a544198)
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd_utils/utils/argcheck.py
+++ deepmd_utils/utils/argcheck.py
@@ -365,7 +365,7 @@ def descrpt_se_atten_common_args():
     doc_seed = "Random seed for parameter initialization"
     doc_exclude_types = "The excluded pairs of types which have no interaction with each other. For example, `[[0, 1]]` means no interaction between type 0 and type 1."
     doc_attn = "The length of hidden vectors in attention layers"
-    doc_attn_layer = "The number of attention layers. Note that model compression of `se_atten` is only enabled when attn_layer==0 and stripped_type_embedding is True"
+    doc_attn_layer = "The number of attention layers."
     doc_attn_dotr = "Whether to do dot product with the normalized relative coordinates"
     doc_attn_mask = "Whether to do mask on the diagonal in the attention matrix"
 
