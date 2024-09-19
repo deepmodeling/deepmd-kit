a9aad688c0555d918c18cf1960ad200d8d662d68
Han Wang <92130845+wanghan-iapcm@users.noreply.github.com>
2024-09-19 20:29:51 +0800

    chore(pt): cleanup deadcode (#4142)
    - #3791
    - mainly preprocess and region
    - deepmd/pt/infer/deep_eval.py:595:0: DC02 Function `eval_model` is
    never used. used by test_unused_params, which seems not easy to remove
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    	- No new features introduced; significant code simplifications made.
      
    - **Bug Fixes**
    - No specific bug fixes identified; however, the removal of outdated
    functions may enhance overall stability.
    
    - **Refactor**
    - Removed the `Region3D` class and associated functions to streamline
    preprocessing logic.
    - Eliminated the `get_atom_axis` function to simplify atom axis
    determination.
    - Removed the `_to_face_distance` function, suggesting a refactor in
    distance calculations.
    
    - **Tests**
    - Removed the `TestLegacyRegion` class, reducing the scope of testing
    related to the `Region3D` functionality.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    Co-authored-by: Han Wang <wang_han@iapcm.ac.cn>

--- source/tests/pt/model/test_region.py
+++ source/tests/pt/model/test_region.py
@@ -4,12 +4,6 @@ import unittest
 import numpy as np
 import torch
 
-from deepmd.pt.utils import (
-    env,
-)
-from deepmd.pt.utils.preprocess import (
-    Region3D,
-)
 from deepmd.pt.utils.region import (
     inter2phys,
     to_face_distance,
@@ -60,29 +54,3 @@ class TestRegion(unittest.TestCase):
                 torch.testing.assert_close(
                     dists[ii][jj], expected, rtol=self.prec, atol=self.prec
                 )
-
-
-class TestLegacyRegion(unittest.TestCase):
-    def setUp(self):
-        self.cell = torch.tensor(
-            [[1, 0, 0], [0.4, 0.8, 0], [0.1, 0.3, 2.1]], dtype=dtype, device=env.DEVICE
-        )
-        self.prec = 1e-6
-
-    def test_inter_to_phys(self):
-        generator = torch.Generator(device=env.DEVICE).manual_seed(GLOBAL_SEED)
-        inter = torch.rand([3, 3], dtype=dtype, device=env.DEVICE, generator=generator)
-        reg = Region3D(self.cell)
-        phys = reg.inter2phys(inter)
-        expected_phys = torch.matmul(inter, self.cell)
-        torch.testing.assert_close(phys, expected_phys, rtol=self.prec, atol=self.prec)
-
-    def test_inter_to_inter(self):
-        generator = torch.Generator(device=env.DEVICE).manual_seed(GLOBAL_SEED)
-        inter = torch.rand([3, 3], dtype=dtype, device=env.DEVICE, generator=generator)
-        reg = Region3D(self.cell)
-        new_inter = reg.phys2inter(reg.inter2phys(inter))
-        torch.testing.assert_close(inter, new_inter, rtol=self.prec, atol=self.prec)
-
-    def test_to_face_dist(self):
-        pass
