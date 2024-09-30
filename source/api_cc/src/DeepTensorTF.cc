d3944bbfaefff1ffa7132b4d7b6d566a2e6e68d4
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-09-30 18:00:42 -0400

    fix(tf): set visible_device_list for TF C++
    Fix #4171.
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/api_cc/src/DeepTensorTF.cc
+++ source/api_cc/src/DeepTensorTF.cc
@@ -46,8 +46,10 @@ void DeepTensorTF::init(const std::string &model,
         0.9);
     options.config.mutable_gpu_options()->set_allow_growth(true);
     DPErrcheck(DPSetDevice(gpu_rank % gpu_num));
-    std::string str = "/gpu:";
-    str += std::to_string(gpu_rank % gpu_num);
+    std::string str = "/gpu:0";
+    // See
+    // https://github.com/tensorflow/tensorflow/blame/8fac27b486939f40bc8e362b94a16a4a8bb51869/tensorflow/core/protobuf/config.proto#L80
+    options.config.visible_device_list = std::to_string(gpu_rank % gpu_num);
     graph::SetDefaultDevice(str, graph_def);
   }
 #endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
