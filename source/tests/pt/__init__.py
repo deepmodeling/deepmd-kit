15e69a89de577735e0e066504574aea9c676f0d3
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-10-07 16:05:15 -0400

    chore: ensure GPU is available in GPU tests
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/tests/pt/__init__.py
+++ source/tests/pt/__init__.py
@@ -1,7 +1,15 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 import torch
 
+from ..common import (
+    TEST_DEVICE,
+)
+
 torch.set_num_threads(1)
 torch.set_num_interop_threads(1)
 # testing purposes; device should always be set explicitly
 torch.set_default_device("cuda:9999999")
+
+# ensure GPU is used when testing GPU code
+if TEST_DEVICE == "cuda":
+    assert torch.cuda.is_available()
--- source/tests/tf/__init__.py
+++ source/tests/tf/__init__.py
@@ -1,1 +1,10 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
+from ..common import (
+    TEST_DEVICE,
+)
+
+# ensure GPU is used when testing GPU code
+if TEST_DEVICE == "cuda":
+    import tensorflow as tf
+
+    assert tf.test.is_gpu_available()
