91ec0847164b299c891130da06a2136a05da736d
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-10-21 17:42:06 -0400

    apply flax_module to energy model
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd/jax/model/ener_model.py
+++ deepmd/jax/model/ener_model.py
@@ -7,12 +7,16 @@ from deepmd.dpmodel.model import EnergyModel as EnergyModelDP
 from deepmd.jax.atomic_model.dp_atomic_model import (
     DPAtomicModel,
 )
+from deepmd.jax.common import (
+    flax_module,
+)
 from deepmd.jax.model.base_model import (
     BaseModel,
 )
 
 
 @BaseModel.register("ener")
+@flax_module
 class EnergyModel(EnergyModelDP):
     def __setattr__(self, name: str, value: Any) -> None:
         if name == "atomic_model":
