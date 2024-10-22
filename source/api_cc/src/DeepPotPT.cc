50621bd8f16f80508e337517868f4ba2e0607f59
Lysithea <ucaszxy@outlook.com>
2024-10-23 00:18:25 +0800

    remove c++ lmp nlist ut due to no comm data

--- source/api_cc/src/DeepPotPT.cc
+++ source/api_cc/src/DeepPotPT.cc
@@ -181,12 +181,8 @@ void DeepPotPT::compute(ENERGYVTYPE& ener,
       torch::Tensor sendnum_tensor =
           torch::from_blob(lmp_list.sendnum, {nswap}, int32_option);
       torch::Tensor communicator_tensor;
-      if (lmp_list.world == 0) {
-        communicator_tensor = torch::empty({1}, torch::kInt64);
-      } else {
-        communicator_tensor = torch::from_blob(
+      communicator_tensor = torch::from_blob(
             const_cast<void*>(lmp_list.world), {1}, torch::kInt64);
-      }
 
       torch::Tensor nswap_tensor = torch::tensor(nswap, int32_option);
       int total_send =
