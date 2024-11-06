94d20543b5c3a1d83eb4636524a9125de343f2f8
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-06 17:04:46 -0500

    nopbc
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/tests/consistent/io/test_io.py
+++ source/tests/consistent/io/test_io.py
@@ -142,6 +142,7 @@ class IOTest:
         nframes = self.atype.shape[0]
         prefix = "test_consistent_io_" + self.__class__.__name__.lower()
         rets = []
+        rets_nopbc = []
         for backend_name, suffix_idx in (
             # unfortunately, jax2tf cannot work with tf v1 behaviors
             ("jax", 2) if DP_TEST_TF2_ONLY else ("tensorflow", 0),
@@ -182,12 +183,38 @@ class IOTest:
                 atomic=True,
             )
             rets.append(ret)
+            ret = deep_eval.eval(
+                self.coords,
+                None,
+                self.atype,
+                fparam=fparam,
+                aparam=aparam,
+            )
+            rets_nopbc.append(ret)
+            ret = deep_eval.eval(
+                self.coords,
+                None,
+                self.atype,
+                fparam=fparam,
+                aparam=aparam,
+                atomic=True,
+            )
+            rets_nopbc.append(ret)
         for ret in rets[1:]:
             for vv1, vv2 in zip(rets[0], ret):
                 if np.isnan(vv2).all():
                     # expect all nan if not supported
                     continue
                 np.testing.assert_allclose(vv1, vv2, rtol=1e-12, atol=1e-12)
+
+        for idx, ret in enumerate(rets_nopbc[1:]):
+            for vv1, vv2 in zip(rets_nopbc[0], ret):
+                if np.isnan(vv2).all():
+                    # expect all nan if not supported
+                    continue
+                np.testing.assert_allclose(
+                    vv1, vv2, rtol=1e-12, atol=1e-12, err_msg=f"backend {idx+1}"
+                )
 
 
 class TestDeepPot(unittest.TestCase, IOTest):
