bd27d4f7f730bcda0401759b68b7857a280e35a1
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-07 02:56:16 -0500

    add tests
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

---
+++ source/tests/jax/jax2tf/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+import pytest
+
+from ...utils import (
+    DP_TEST_TF2_ONLY,
+)
+
+pytest.mark.skipif(not DP_TEST_TF2_ONLY, reason="TF2 conflicts with TF1")
