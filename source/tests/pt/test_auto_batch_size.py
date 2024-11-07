eb7384e57fdc350650c4f99a16d9c398d392bb66
HydrogenSulfate <490868991@qq.com>
2024-11-07 13:07:26 +0800

    restore pt/test_auto_batch_size.py

--- source/tests/pt/test_auto_batch_size.py
+++ source/tests/pt/test_auto_batch_size.py
@@ -2,7 +2,6 @@
 import unittest
 
 import numpy as np
-import torch
 
 from deepmd.pt.utils.auto_batch_size import (
     AutoBatchSize,
@@ -11,28 +10,28 @@ from deepmd.pt.utils.auto_batch_size import (
 
 class TestAutoBatchSize(unittest.TestCase):
     def test_execute_all(self):
-        dd0 = torch.zeros((10000, 2, 1, 3, 4))
-        dd1 = torch.ones((10000, 2, 1, 3, 4))
+        dd0 = np.zeros((10000, 2, 1, 3, 4))
+        dd1 = np.ones((10000, 2, 1, 3, 4))
         auto_batch_size = AutoBatchSize(256, 2.0)
 
         def func(dd1):
-            return torch.zeros_like(dd1), torch.ones_like(dd1)
+            return np.zeros_like(dd1), np.ones_like(dd1)
 
         dd2 = auto_batch_size.execute_all(func, 10000, 2, dd1)
-        np.testing.assert_equal(dd0.cpu().numpy(), dd2[0].cpu().numpy())
-        np.testing.assert_equal(dd1.cpu().numpy(), dd2[1].cpu().numpy())
+        np.testing.assert_equal(dd0, dd2[0])
+        np.testing.assert_equal(dd1, dd2[1])
 
     def test_execute_all_dict(self):
-        dd0 = torch.zeros((10000, 2, 1, 3, 4))
-        dd1 = torch.ones((10000, 2, 1, 3, 4))
+        dd0 = np.zeros((10000, 2, 1, 3, 4))
+        dd1 = np.ones((10000, 2, 1, 3, 4))
         auto_batch_size = AutoBatchSize(256, 2.0)
 
         def func(dd1):
             return {
-                "foo": torch.zeros_like(dd1),
-                "bar": torch.ones_like(dd1),
+                "foo": np.zeros_like(dd1),
+                "bar": np.ones_like(dd1),
             }
 
         dd2 = auto_batch_size.execute_all(func, 10000, 2, dd1)
-        np.testing.assert_equal(dd0.cpu().numpy(), dd2["foo"].cpu().numpy())
-        np.testing.assert_equal(dd1.cpu().numpy(), dd2["bar"].cpu().numpy())
+        np.testing.assert_equal(dd0, dd2["foo"])
+        np.testing.assert_equal(dd1, dd2["bar"])
