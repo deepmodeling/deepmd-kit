050c20c6044839795c4c56f1562a31b2bbb78bd0
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-08 15:23:34 -0500

    set xla flags before any imports
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/tests/__init__.py
+++ source/tests/__init__.py
@@ -1,1 +1,11 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
+import os
+
+# set XLA FLAGS before any jax import
+os.environ["XLA_FLAGS"] = " ".join(
+    (
+        "--xla_cpu_multi_thread_eigen=false",
+        "intra_op_parallelism_threads=1",
+        "inter_op_parallelism_threads=1",
+    )
+)
