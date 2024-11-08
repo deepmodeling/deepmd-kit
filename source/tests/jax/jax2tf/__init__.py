31f9e8772b4ee719591c2fdfa0bc5c1f229f748e
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-08 13:02:21 -0500

    limit threads during tests
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/tests/jax/jax2tf/__init__.py
+++ source/tests/jax/jax2tf/__init__.py
@@ -1,1 +1,6 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
+import tensorflow as tf
+
+# limit the number of threads
+tf.config.threading.set_inter_op_parallelism_threads(1)
+tf.config.threading.set_intra_op_parallelism_threads(1)
