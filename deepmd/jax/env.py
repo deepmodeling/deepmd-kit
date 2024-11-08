00e3c10ddaaa015b92846074ebf6c8eada13d276
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-08 13:12:45 -0500

    fix(jax): set threads for XLA and TF
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd/jax/env.py
+++ deepmd/jax/env.py
@@ -1,6 +1,18 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 import os
 
+from deepmd.env import (
+    get_default_nthreads,
+    set_default_nthreads,
+)
+
+set_default_nthreads()
+inter_nthreads, intra_nthreads = get_default_nthreads()
+os.environ["XLA_FLAGS"] = os.environ.get("XLA_FLAGS", "") + (
+    " --xla_cpu_multi_thread_eigen=false"
+    f" intra_op_parallelism_threads={inter_nthreads}"
+    f" inter_op_parallelism_threads={inter_nthreads}"
+)
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 
 import jax
--- deepmd/jax/jax2tf/__init__.py
+++ deepmd/jax/jax2tf/__init__.py
@@ -1,6 +1,11 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 import tensorflow as tf
 
+from deepmd.env import (
+    get_default_nthreads,
+    set_default_nthreads,
+)
+
 if not tf.executing_eagerly():
     # TF disallow temporary eager execution
     raise RuntimeError(
@@ -9,3 +14,8 @@ if not tf.executing_eagerly():
         "If you are converting a model between different backends, "
         "considering converting to the `.dp` format first."
     )
+
+set_default_nthreads()
+inter_nthreads, intra_nthreads = get_default_nthreads()
+tf.config.threading.set_inter_op_parallelism_threads(inter_nthreads)
+tf.config.threading.set_intra_op_parallelism_threads(intra_nthreads)
