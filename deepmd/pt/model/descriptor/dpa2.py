51e86964c8c104aa18b4a3918d6871076a871f3c
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-08 21:42:18 -0500

    fix typo in docstring
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd/pt/model/descriptor/dpa2.py
+++ deepmd/pt/model/descriptor/dpa2.py
@@ -873,7 +873,7 @@ class DescrptDPA2(BaseDescriptor, torch.nn.Module):
         table_stride_2: float = 0.1,
         check_frequency: int = -1,
     ) -> None:
-        """Receive the statisitcs (distance, max_nbor_size and env_mat_range) of the training data.
+        """Receive the statistics (distance, max_nbor_size and env_mat_range) of the training data.
 
         Parameters
         ----------
