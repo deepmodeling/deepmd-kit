b9ff7555e61f0564f6011d19ce06968ce77d319b
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-08 20:07:37 -0500

    try scope module

--- source/tests/conftest.py
+++ source/tests/conftest.py
@@ -4,7 +4,7 @@ import gc
 import pytest
 
 
-@pytest.fixture(scope="package", autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def automatic_memory_release():
     """Release memory after each package."""
     # pre
