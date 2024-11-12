93cc440415523dac3198f84eccc62fe0bf630487
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-12 16:10:35 -0500

    add script path
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/tests/infer/convert-models.sh
+++ source/tests/infer/convert-models.sh
@@ -2,5 +2,7 @@
 
 set -ev
 
-dp convert-backend deeppot_sea.yaml deeppot_sea.savedmodel
-dp convert-backend deeppot_dpa.yaml deeppot_dpa.savedmodel
+SCRIPT_PATH=$(dirname $(realpath -s $0))
+
+dp convert-backend ${SCRIPT_PATH}/deeppot_sea.yaml ${SCRIPT_PATH}/deeppot_sea.savedmodel
+dp convert-backend ${SCRIPT_PATH}/deeppot_dpa.yaml ${SCRIPT_PATH}/deeppot_dpa.savedmodel
