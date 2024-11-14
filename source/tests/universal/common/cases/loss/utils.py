f35e5422e9b36213f5e606f537bec3a4999842a9
Duo <50307526+iProzd@users.noreply.github.com>
2024-11-14 17:12:33 +0800

    Update utils.py

--- source/tests/universal/common/cases/loss/utils.py
+++ source/tests/universal/common/cases/loss/utils.py
@@ -33,19 +33,24 @@ class LossTestCase:
         label_dict = {item.key: item for item in label_requirement}
         label_keys = sorted(label_dict.keys())
         natoms = 5
+        nframes = 2
 
         def fake_model():
             model_predict = {
-                data_key: fake_input_one_frame(label_dict[data_key], natoms)
+                data_key: fake_input(
+                    label_dict[data_key], natoms=natoms, nframes=nframes
+                )
                 for data_key in label_keys
             }
             if "atom_ener" in model_predict:
                 model_predict["atom_energy"] = model_predict.pop("atom_ener")
-            model_predict.update({"mask_mag": np.ones([1, natoms, 1], dtype=np.bool_)})
+            model_predict.update(
+                {"mask_mag": np.ones([nframes, natoms, 1], dtype=np.bool_)}
+            )
             return model_predict
 
         labels = {
-            data_key: fake_input_one_frame(label_dict[data_key], natoms)
+            data_key: fake_input(label_dict[data_key], natoms=natoms, nframes=nframes)
             for data_key in label_keys
         }
         labels.update({"find_" + data_key: 1.0 for data_key in label_keys})
@@ -59,12 +64,11 @@ class LossTestCase:
         )
 
 
-def fake_input_one_frame(data_item: DataRequirementItem, natoms=5) -> np.ndarray:
+def fake_input(data_item: DataRequirementItem, natoms=5, nframes=2) -> np.ndarray:
     ndof = data_item.ndof
     atomic = data_item.atomic
     repeat = data_item.repeat
     rng = np.random.default_rng(seed=GLOBAL_SEED)
-    nframes = 1
     dtype = data_item.dtype if data_item.dtype is not None else np.float64
     if atomic:
         data = rng.random([nframes, natoms, ndof], dtype)
