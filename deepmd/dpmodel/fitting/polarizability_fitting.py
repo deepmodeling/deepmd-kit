fix(dpmodel): fix precision (#4343)
<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

## Release Notes

- **New Features**
- Introduced a new environment variable `DP_DTYPE_PROMOTION_STRICT` to
enhance precision handling in TensorFlow tests.
- Added a decorator `@cast_precision` to several descriptor classes,
improving precision management during computations.
- Updated JAX configuration to enable strict dtype promotion based on
the new environment variable.
- Enhanced serialization and deserialization processes to include
precision attributes across multiple classes.

- **Bug Fixes**
- Enhanced type handling and input processing in the `GeneralFitting`
class for better output predictions.
- Improved handling of atomic contributions and exclusions in the
`BaseAtomicModel` class.
- Addressed potential type mismatches during matrix operations in the
`NativeLayer` class.

- **Chores**
- Updated caching mechanisms in the testing workflow to ensure unique
keys based on run parameters.
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

---------

Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
Co-authored-by: Han Wang <92130845+wanghan-iapcm@users.noreply.github.com>
Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

deepmd/dpmodel/fitting/polarizability_fitting.py
@@ -15,6 +15,7 @@ from deepmd.dpmodel import (
     DEFAULT_PRECISION,
 )
 from deepmd.dpmodel.common import (
+    cast_precision,
     to_numpy_array,
 )
 from deepmd.dpmodel.fitting.base_fitting import (
@@ -241,6 +242,7 @@ class PolarFitting(GeneralFitting):
         self.scale = self.scale[remap_index]
         self.constant_matrix = self.constant_matrix[remap_index]
 
+    @cast_precision
     def call(
         self,
         descriptor: np.ndarray,
@@ -285,7 +287,8 @@ class PolarFitting(GeneralFitting):
         ]
         # out = out * self.scale[atype, ...]
         scale_atype = xp.reshape(
-            xp.take(self.scale, xp.reshape(atype, [-1]), axis=0), (*atype.shape, 1)
+            xp.take(xp.astype(self.scale, out.dtype), xp.reshape(atype, [-1]), axis=0),
+            (*atype.shape, 1),
         )
         out = out * scale_atype
         # (nframes * nloc, m1, 3)
@@ -308,7 +311,11 @@ class PolarFitting(GeneralFitting):
         if self.shift_diag:
             # bias = self.constant_matrix[atype]
             bias = xp.reshape(
-                xp.take(self.constant_matrix, xp.reshape(atype, [-1]), axis=0),
+                xp.take(
+                    xp.astype(self.constant_matrix, out.dtype),
+                    xp.reshape(atype, [-1]),
+                    axis=0,
+                ),
                 (nframes, nloc),
             )
             # (nframes, nloc, 1)