abb32d1a0829bb8d4b81a934b2dc94e2acecd5c1
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-21 14:05:44 -0500

    chore: optimize compute_smooth_weight (#4390)
    New implmenetation is obviously more efficient.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced the `compute_smooth_weight` functionality for improved
    efficiency and clarity by simplifying the distance handling logic.
    - Introduced configuration for the `array_api_strict` module to ensure
    compatibility with the latest API version.
    
    - **Bug Fixes**
    - Removed unnecessary masking conditions, ensuring smoother calculations
    within defined distance ranges.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- deepmd/pt/utils/preprocess.py
+++ deepmd/pt/utils/preprocess.py
@@ -10,9 +10,8 @@ def compute_smooth_weight(distance, rmin: float, rmax: float):
     """Compute smooth weight for descriptor elements."""
     if rmin >= rmax:
         raise ValueError("rmin should be less than rmax.")
-    min_mask = distance <= rmin
-    max_mask = distance >= rmax
-    mid_mask = torch.logical_not(torch.logical_or(min_mask, max_mask))
+    distance = torch.clamp(distance, min=rmin, max=rmax)
     uu = (distance - rmin) / (rmax - rmin)
-    vv = uu * uu * uu * (-6 * uu * uu + 15 * uu - 10) + 1
-    return vv * mid_mask + min_mask
+    uu2 = uu * uu
+    vv = uu2 * uu * (-6 * uu2 + 15 * uu - 10) + 1
+    return vv
