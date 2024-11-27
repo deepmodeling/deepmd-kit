0fa2c6deea98a8407b304f9a77d46a2de6594ec2
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-27 15:00:11 -0500

    fix: unmark `eval_pd` as `abstractmethod`
    Many classes don't have this method and become abstract classes. It looks to me these tests are skipped cilently and don't throw errors.
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/tests/consistent/common.py
+++ source/tests/consistent/common.py
@@ -185,7 +185,6 @@ class CommonTest(ABC):
         """
         raise NotImplementedError("Not implemented")
 
-    @abstractmethod
     def eval_pd(self, pd_obj: Any) -> Any:
         """Evaluate the return value of PD.
 
@@ -194,6 +193,7 @@ class CommonTest(ABC):
         pd_obj : Any
             The object of PD
         """
+        raise NotImplementedError("Not implemented")
 
     def eval_array_api_strict(self, array_api_strict_obj: Any) -> Any:
         """Evaluate the return value of array_api_strict.
