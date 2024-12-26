chore: fix spelling PRECISON -> PRECISION (#4508)
Luckily, this variable has never been used otherwhere.

<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->

## Summary by CodeRabbit

- **Bug Fixes**
- Corrected the spelling of `RESERVED_PRECISON_DICT` to
`RESERVED_PRECISION_DICT` across multiple files to ensure consistency
and prevent potential runtime errors.
  
- **Documentation**
- Updated comments and documentation to reflect the corrected variable
name, enhancing clarity without altering functionality.

<!-- end of auto-generated comment: release notes by coderabbit.ai -->

deepmd/dpmodel/common.py
@@ -43,7 +43,7 @@ PRECISION_DICT = {
 }
 assert VALID_PRECISION.issubset(PRECISION_DICT.keys())
 
-RESERVED_PRECISON_DICT = {
+RESERVED_PRECISION_DICT = {
     np.float16: "float16",
     np.float32: "float32",
     np.float64: "float64",
@@ -52,7 +52,7 @@ RESERVED_PRECISON_DICT = {
     ml_dtypes.bfloat16: "bfloat16",
     np.bool_: "bool",
 }
-assert set(RESERVED_PRECISON_DICT.keys()) == set(PRECISION_DICT.values())
+assert set(RESERVED_PRECISION_DICT.keys()) == set(PRECISION_DICT.values())
 DEFAULT_PRECISION = "float64"
 
 
@@ -74,9 +74,9 @@ def get_xp_precision(
     elif precision == "bool":
         return bool
     elif precision == "default":
-        return get_xp_precision(xp, RESERVED_PRECISON_DICT[PRECISION_DICT[precision]])
+        return get_xp_precision(xp, RESERVED_PRECISION_DICT[PRECISION_DICT[precision]])
     elif precision == "global":
-        return get_xp_precision(xp, RESERVED_PRECISON_DICT[GLOBAL_NP_FLOAT_PRECISION])
+        return get_xp_precision(xp, RESERVED_PRECISION_DICT[GLOBAL_NP_FLOAT_PRECISION])
     elif precision == "bfloat16":
         return ml_dtypes.bfloat16
     else:
@@ -225,6 +225,6 @@ __all__ = [
     "GLOBAL_ENER_FLOAT_PRECISION",
     "GLOBAL_NP_FLOAT_PRECISION",
     "PRECISION_DICT",
-    "RESERVED_PRECISON_DICT",
+    "RESERVED_PRECISION_DICT",
     "NativeOP",
 ]

deepmd/pt/model/descriptor/se_a.py
@@ -23,7 +23,7 @@ from deepmd.pt.utils import (
 )
 from deepmd.pt.utils.env import (
     PRECISION_DICT,
-    RESERVED_PRECISON_DICT,
+    RESERVED_PRECISION_DICT,
 )
 from deepmd.pt.utils.env_mat_stat import (
     EnvMatStatSe,
@@ -379,7 +379,7 @@ class DescrptSeA(BaseDescriptor, torch.nn.Module):
             "set_davg_zero": obj.set_davg_zero,
             "activation_function": obj.activation_function,
             # make deterministic
-            "precision": RESERVED_PRECISON_DICT[obj.prec],
+            "precision": RESERVED_PRECISION_DICT[obj.prec],
             "embeddings": obj.filter_layers.serialize(),
             "env_mat": DPEnvMat(obj.rcut, obj.rcut_smth).serialize(),
             "exclude_types": obj.exclude_types,