088450e0017a1bcafae8d4fa409db314d7c7a8fd
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2025-01-08 16:46:34 +0800

    Apply suggestions from code review
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/tests/tf/test_dipole_hybrid_descrpt.py
+++ source/tests/tf/test_dipole_hybrid_descrpt.py
@@ -35,8 +35,6 @@ class TestModel(tf.test.TestCase):
 
         systems = jdata["systems"]
         set_pfx = "set"
-        batch_size = jdata["batch_size"]
-        test_size = jdata["numb_test"]
         batch_size = 1
         test_size = 1
         rcut = jdata["model"]["descriptor"]["rcut"]
