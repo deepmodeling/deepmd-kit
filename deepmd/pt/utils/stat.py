10194d0e606fc9af096db82b8d878d1a64d11367
sum <guo0808@emails.bjut.edu.cn>
2025-02-12 14:54:21 +0800

    no assert

--- deepmd/pt/utils/stat.py
+++ deepmd/pt/utils/stat.py
@@ -164,14 +164,6 @@ def make_stat_input(
                 newele_counter += 1
                 if newele_counter <= min_frames_per_element_forstat:
                     frame_data = sys.__getitem__(frame)
-                    if miss not in frame_data["atype"]:
-                        raise ValueError(  
-                            "Element check failed. "
-                            "If you are running in 'change-bias' mode, use '--skip-elementcheck' to disable this check. "
-                            "If you encountered this error during model training, set 'enable_element_completion' to False "
-                            "in the 'training' section of your input file."
-                        )
-
                     sys_stat_new = {}
                     for dd in frame_data:
                         if dd == "type":
