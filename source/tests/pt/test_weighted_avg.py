5a3b942163a7b1e8f15642693270bfe190aab45d
sum <guo0808@emails.bjut.edu.cn>
2025-02-26 19:16:36 +0800

    make ut cover all

--- source/tests/pt/test_weighted_avg.py
+++ source/tests/pt/test_weighted_avg.py
@@ -25,7 +25,7 @@ from deepmd.infer.deep_eval import (
     DeepEval,
 )
 
-class Test_testener(unittest.TestCase):
+class Test_testener_without_spin(unittest.TestCase):
     def setUp(self) -> None:
         self.detail_file = "test_dp_test_ener_detail"
         input_json = str(Path(__file__).parent / "water/se_atten.json")
@@ -60,15 +60,11 @@ class Test_testener(unittest.TestCase):
         with open(self.input_json, "w") as fp:
             json.dump(self.config, fp, indent=4)
         trainer = get_trainer(deepcopy(self.config))
-        with torch.device("cpu"):
-            
-            input_dict, _, _ = trainer.get_data(is_train=False)
-        input_dict.pop("spin", None)
         model = torch.jit.script(trainer.model)
         self.tmp_model = tempfile.NamedTemporaryFile(delete=False, suffix=".pth")
         torch.jit.save(model, self.tmp_model.name)
 
-    def test_dp_test_1_frame(self) -> None:
+    def test_dp_test_ener_without_spin(self) -> None:
         dp = DeepEval(self.tmp_model.name,head='PyTorch')
         system = self.config["training"]["validation_data"]["systems"][0]
         data = DeepmdData(
@@ -81,7 +77,85 @@ class Test_testener(unittest.TestCase):
         err = dp_test_ener(
                 dp,
                 data,
-                system=self.config["training"]["validation_data"]["systems"],
+                system,
+                numb_test=1,
+                detail_file=None,
+                has_atom_ener=False,
+            )
+        print(err)
+        os.unlink(self.tmp_model.name)
+
+    def tearDown(self) -> None:
+        for f in os.listdir("."):
+            if f.startswith("model") and f.endswith(".pt"):
+                os.remove(f)
+            if f.startswith(self.detail_file):
+                os.remove(f)
+            if f in ["lcurve.out", self.input_json]:
+                os.remove(f)
+            if f in ["stat_files"]:
+                shutil.rmtree(f)
+
+class Test_testener_spin(unittest.TestCase):
+    def setUp(self) -> None:
+        self.detail_file = "test_dp_test_ener_spin_detail"
+        input_json = str(Path(__file__).parent / "water/se_atten.json")
+        with open(input_json) as f:
+            self.config = json.load(f)
+        self.config["training"]["numb_steps"] = 1
+        self.config["training"]["save_freq"] = 1
+        data_file = [str(Path(__file__).parent / "NiO/data/single")]
+        self.config["training"]["training_data"]["systems"] = data_file
+        self.config["training"]["validation_data"]["systems"] = data_file
+        model_spin=model_spin = {
+                    "type_map": ["O", "H", "B"],
+                    "descriptor": {
+                        "type": "se_e2_a",
+                        "sel": [46, 92, 4],
+                        "rcut_smth": 0.50,
+                        "rcut": 4.00,
+                        "neuron": [25, 50, 100],
+                        "resnet_dt": False,
+                        "axis_neuron": 16,
+                        "seed": 1,
+                    },
+                    "fitting_net": {
+                        "neuron": [24, 24, 24],
+                        "resnet_dt": True,
+                        "seed": 1,
+                    },
+                    "data_stat_nbatch": 20,
+                    "spin": {
+                        "use_spin": [True, False, False],
+                        "virtual_scale": [0.3140],
+                        "_comment": " that's all",
+                    },
+                }
+        self.config["model"] = deepcopy(model_spin)
+        self.config["model"]["type_map"] = ["Ni", "O", "B"]
+        self.input_json = "test_dp_test.json"
+        with open(self.input_json, "w") as fp:
+            json.dump(self.config, fp, indent=4)
+        trainer = get_trainer(deepcopy(self.config))
+        model = torch.jit.script(trainer.model)
+        self.tmp_model = tempfile.NamedTemporaryFile(delete=False, suffix=".pth")
+        torch.jit.save(model, self.tmp_model.name)
+
+    def test_dp_test_ener_with_spin(self) -> None:
+        dp = DeepEval(self.tmp_model.name,head='PyTorch')
+        system = self.config["training"]["validation_data"]["systems"][0]
+        data = DeepmdData(
+            sys_path=system,
+            set_prefix="set",
+            shuffle_test=False,
+            type_map=dp.get_type_map(),
+            sort_atoms=False,
+        )
+
+        err = dp_test_ener(
+                dp,
+                data,
+                system,
                 numb_test=1,
                 detail_file=None,
                 has_atom_ener=False,
