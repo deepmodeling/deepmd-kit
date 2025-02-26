c4e6fcfeb98ef6402ee288e7f30c0f2f92a6ce95
sum <guo0808@emails.bjut.edu.cn>
2025-02-26 21:27:15 +0800

    make ut cover multisys

--- source/tests/pt/test_weighted_avg.py
+++ source/tests/pt/test_weighted_avg.py
@@ -25,9 +25,8 @@ from deepmd.utils.data import (
     DeepmdData,
 )
 
-from .model.test_permutation import (
-    model_se_e2_a,
-    model_spin,
+from deepmd.utils.weight_avg import (
+    weighted_average,
 )
 
 
@@ -42,6 +41,26 @@ class Test_testener_without_spin(unittest.TestCase):
         data_file = [str(Path(__file__).parent / "water/data/single")]
         self.config["training"]["training_data"]["systems"] = data_file
         self.config["training"]["validation_data"]["systems"] = data_file
+        model_se_e2_a = {
+    "type_map": ["O", "H", "B"],
+    "descriptor": {
+        "type": "se_e2_a",
+        "sel": [46, 92, 4],
+        "rcut_smth": 0.50,
+        "rcut": 4.00,
+        "neuron": [25, 50, 100],
+        "resnet_dt": False,
+        "axis_neuron": 16,
+        "seed": 1,
+    },
+    "fitting_net": {
+        "neuron": [24, 24, 24],
+        "resnet_dt": True,
+        "seed": 1,
+    },
+    "data_stat_nbatch": 20,
+}
+
         self.config["model"] = deepcopy(model_se_e2_a)
         self.input_json = "test_dp_test.json"
         with open(self.input_json, "w") as fp:
@@ -101,6 +120,25 @@ class Test_testener_with_virial(unittest.TestCase):
         data_file = [str(Path(__file__).parent / "water/data/single")]
         self.config["training"]["training_data"]["systems"] = data_file
         self.config["training"]["validation_data"]["systems"] = data_file
+        model_se_e2_a = {
+    "type_map": ["O", "H", "B"],
+    "descriptor": {
+        "type": "se_e2_a",
+        "sel": [46, 92, 4],
+        "rcut_smth": 0.50,
+        "rcut": 4.00,
+        "neuron": [25, 50, 100],
+        "resnet_dt": False,
+        "axis_neuron": 16,
+        "seed": 1,
+    },
+    "fitting_net": {
+        "neuron": [24, 24, 24],
+        "resnet_dt": True,
+        "seed": 1,
+    },
+    "data_stat_nbatch": 20,
+}
         self.config["model"] = deepcopy(model_se_e2_a)
         self.input_json = "test_dp_test.json"
         with open(self.input_json, "w") as fp:
@@ -158,6 +196,116 @@ class Test_testener_with_virial(unittest.TestCase):
             if os.path.exists(virial_path_fake):
                 os.remove(virial_path_fake)
 
+class Test_testener_with_multisys(unittest.TestCase):
+    def setUp(self) -> None:
+        self.detail_file = "test_dp_test_ener_detail"
+        input_json = str(Path(__file__).parent / "water/se_atten.json")
+        with open(input_json) as f:
+            self.config = json.load(f)
+        self.config["training"]["numb_steps"] = 1
+        self.config["training"]["save_freq"] = 1
+        data_file = [str(Path(__file__).parent / "water/data/single")]
+        self.config["training"]["training_data"]["systems"] = data_file
+        self.config["training"]["validation_data"]["systems"] = data_file
+        model_se_e2_a = {
+    "type_map": ["O", "H", "B"],
+    "descriptor": {
+        "type": "se_e2_a",
+        "sel": [46, 92, 4],
+        "rcut_smth": 0.50,
+        "rcut": 4.00,
+        "neuron": [25, 50, 100],
+        "resnet_dt": False,
+        "axis_neuron": 16,
+        "seed": 1,
+    },
+    "fitting_net": {
+        "neuron": [24, 24, 24],
+        "resnet_dt": True,
+        "seed": 1,
+    },
+    "data_stat_nbatch": 20,
+}
+        self.config["model"] = deepcopy(model_se_e2_a)
+        self.input_json = "test_dp_test.json"
+        with open(self.input_json, "w") as fp:
+            json.dump(self.config, fp, indent=4)
+        trainer = get_trainer(deepcopy(self.config))
+        model = torch.jit.script(trainer.model)
+        self.tmp_model = tempfile.NamedTemporaryFile(delete=False, suffix=".pth")
+        torch.jit.save(model, self.tmp_model.name)
+
+    def test_dp_test_ener_with_multisys(self) -> None:
+        dp = DeepEval(self.tmp_model.name, head="PyTorch")
+        system = self.config["training"]["validation_data"]["systems"][0]
+        data = DeepmdData(
+            sys_path=system,
+            set_prefix="set",
+            shuffle_test=False,
+            type_map=dp.get_type_map(),
+            sort_atoms=False,
+        )
+        err = []
+        err_novirial = dp_test_ener(
+            dp,
+            data,
+            system,
+            numb_test=1,
+            detail_file=None,
+            has_atom_ener=False,
+        )
+        err.append(err_novirial)
+        ener_nv,weight_nv = err_novirial['mae_e']
+        virial_path_fake = os.path.join(
+            self.config["training"]["validation_data"]["systems"][0],
+            "set.000",
+            "virial.npy",
+        )
+        np.save(virial_path_fake, np.ones([1, 9], dtype=np.float64))
+        data = DeepmdData(
+            sys_path=system,
+            set_prefix="set",
+            shuffle_test=False,
+            type_map=dp.get_type_map(),
+            sort_atoms=False,
+        )
+        err_virial = dp_test_ener(
+            dp,
+            data,
+            system,
+            numb_test=1,
+            detail_file=None,
+            has_atom_ener=False,
+        )
+        ener_v, weight_v = err_virial['mae_e']
+        mae_v,_ = err_virial['mae_v']
+        weight=weight_nv+weight_v
+        ener=(ener_v*weight_v)+(ener_nv*weight_nv)
+        mae_e_expected = ener/weight
+        err.append(err_virial)
+        avg_err = weighted_average(err)
+        assert avg_err["mae_v"] == mae_v, f"Expected mae_v in avg_err to be {mae_v} but got {avg_err['mae_v']}"
+        assert avg_err["mae_e"] == mae_e_expected, f"Expected mae_e in avg_err to be {mae_e_expected} but got {avg_err['mae_e']}"
+
+        os.unlink(self.tmp_model.name)
+
+    def tearDown(self) -> None:
+        for f in os.listdir("."):
+            if f.startswith("model") and f.endswith(".pt"):
+                os.remove(f)
+            if f.startswith(self.detail_file):
+                os.remove(f)
+            if f in ["lcurve.out", self.input_json]:
+                os.remove(f)
+            if f in ["stat_files"]:
+                shutil.rmtree(f)
+            virial_path_fake = os.path.join(
+                self.config["training"]["validation_data"]["systems"][0],
+                "set.000",
+                "virial.npy",
+            )
+            if os.path.exists(virial_path_fake):
+                os.remove(virial_path_fake)
 
 class Test_testener_spin(unittest.TestCase):
     def setUp(self) -> None:
@@ -170,6 +318,30 @@ class Test_testener_spin(unittest.TestCase):
         data_file = [str(Path(__file__).parent / "NiO/data/single")]
         self.config["training"]["training_data"]["systems"] = data_file
         self.config["training"]["validation_data"]["systems"] = data_file
+        model_spin = {
+    "type_map": ["O", "H", "B"],
+    "descriptor": {
+        "type": "se_e2_a",
+        "sel": [46, 92, 4],
+        "rcut_smth": 0.50,
+        "rcut": 4.00,
+        "neuron": [25, 50, 100],
+        "resnet_dt": False,
+        "axis_neuron": 16,
+        "seed": 1,
+    },
+    "fitting_net": {
+        "neuron": [24, 24, 24],
+        "resnet_dt": True,
+        "seed": 1,
+    },
+    "data_stat_nbatch": 20,
+    "spin": {
+        "use_spin": [True, False, False],
+        "virtual_scale": [0.3140],
+        "_comment": " that's all",
+    },
+}
         self.config["model"] = deepcopy(model_spin)
         self.config["model"]["type_map"] = ["Ni", "O", "B"]
         self.input_json = "test_dp_test.json"
