8d17582dc69f68872deecd7df790dccee2234a00
sum <guo0808@emails.bjut.edu.cn>
2025-02-26 19:47:06 +0800

    make ut with virial

--- source/tests/pt/test_weighted_avg.py
+++ source/tests/pt/test_weighted_avg.py
@@ -23,6 +23,7 @@ from deepmd.pt.entrypoints.main import (
 from deepmd.utils.data import (
     DeepmdData,
 )
+import numpy as np
 
 
 class Test_testener_without_spin(unittest.TestCase):
@@ -82,7 +83,11 @@ class Test_testener_without_spin(unittest.TestCase):
                 detail_file=None,
                 has_atom_ener=False,
             )
-        print(err)
+        self.assertIn("mae_e", err, "'mae_e' key is missing in the result")
+        self.assertNotIn("mae_fm", err, "'mae_fm' key is missing in the result")
+        self.assertNotIn("mae_v", err, "'mae_v' key should not be present in the result")
+        self.assertIn("mae_f", err, "'mae_f' key should not be present in the result")
+    
         os.unlink(self.tmp_model.name)
 
     def tearDown(self) -> None:
@@ -96,6 +101,86 @@ class Test_testener_without_spin(unittest.TestCase):
             if f in ["stat_files"]:
                 shutil.rmtree(f)
 
+class Test_testener_with_virial(unittest.TestCase):
+    def setUp(self) -> None:
+        self.detail_file = "test_dp_test_ener_detail"
+        input_json = str(Path(__file__).parent / "water/se_atten.json")
+        with open(input_json) as f:
+            self.config = json.load(f)
+        self.config["training"]["numb_steps"] = 1
+        self.config["training"]["save_freq"] = 1
+        data_file = [str(Path(__file__).parent / "water/data/single")]
+        self.config["training"]["training_data"]["systems"] = data_file
+        self.config["training"]["validation_data"]["systems"] = data_file
+        model_se_e2_a = {
+            "type_map": ["O", "H", "B"],
+            "descriptor": {
+                "type": "se_e2_a",
+                "sel": [46, 92, 4],
+                "rcut_smth": 0.50,
+                "rcut": 4.00,
+                "neuron": [25, 50, 100],
+                "resnet_dt": False,
+                "axis_neuron": 16,
+                "seed": 1,
+            },
+            "fitting_net": {
+                "neuron": [24, 24, 24],
+                "resnet_dt": True,
+                "seed": 1,
+            },
+            "data_stat_nbatch": 20,
+        }
+        self.config["model"] = deepcopy(model_se_e2_a)
+        self.input_json = "test_dp_test.json"
+        with open(self.input_json, "w") as fp:
+            json.dump(self.config, fp, indent=4)
+        trainer = get_trainer(deepcopy(self.config))
+        model = torch.jit.script(trainer.model)
+        self.tmp_model = tempfile.NamedTemporaryFile(delete=False, suffix=".pth")
+        torch.jit.save(model, self.tmp_model.name)
+
+    def test_dp_test_ener_with_virial(self) -> None:
+        virial_path_fake = os.path.join(self.config["training"]["validation_data"]["systems"][0], 'set.000', 'virial.npy')
+        np.save(virial_path_fake,np.ones([1, 9], dtype=np.float64))
+        dp = DeepEval(self.tmp_model.name,head='PyTorch')
+        system = self.config["training"]["validation_data"]["systems"][0]
+        data = DeepmdData(
+            sys_path=system,
+            set_prefix="set",
+            shuffle_test=False,
+            type_map=dp.get_type_map(),
+            sort_atoms=False,
+        )
+        err = dp_test_ener(
+                dp,
+                data,
+                system,
+                numb_test=1,
+                detail_file=None,
+                has_atom_ener=False,
+            )
+        self.assertIn("mae_e", err, "'mae_e' key is missing in the result")
+        self.assertNotIn("mae_fm", err, "'mae_fm' key is missing in the result")
+        self.assertIn("mae_v", err, "'mae_v' key should not be present in the result")
+        self.assertIn("mae_f", err, "'mae_f' key should not be present in the result")
+    
+        os.unlink(self.tmp_model.name)
+
+    def tearDown(self) -> None:
+        for f in os.listdir("."):
+            if f.startswith("model") and f.endswith(".pt"):
+                os.remove(f)
+            if f.startswith(self.detail_file):
+                os.remove(f)
+            if f in ["lcurve.out", self.input_json]:
+                os.remove(f)
+            if f in ["stat_files"]:
+                shutil.rmtree(f)
+            virial_path_fake = os.path.join(self.config["training"]["validation_data"]["systems"][0], 'set.000', 'virial.npy')
+            if os.path.exists(virial_path_fake):
+                os.remove(virial_path_fake)
+                
 class Test_testener_spin(unittest.TestCase):
     def setUp(self) -> None:
         self.detail_file = "test_dp_test_ener_spin_detail"
@@ -160,7 +245,10 @@ class Test_testener_spin(unittest.TestCase):
                 detail_file=None,
                 has_atom_ener=False,
             )
-        print(err)
+        self.assertIn("mae_e", err, "'mae_e' key is missing in the result")
+        self.assertIn("mae_fm", err, "'mae_fm' key is missing in the result")
+        self.assertNotIn("mae_v", err, "'mae_v' key should not be present in the result")
+        self.assertNotIn("mae_f", err, "'mae_f' key should not be present in the result")
         os.unlink(self.tmp_model.name)
 
     def tearDown(self) -> None:
@@ -173,7 +261,6 @@ class Test_testener_spin(unittest.TestCase):
                 os.remove(f)
             if f in ["stat_files"]:
                 shutil.rmtree(f)
-
 
 if __name__ == "__main__":
     unittest.main()
