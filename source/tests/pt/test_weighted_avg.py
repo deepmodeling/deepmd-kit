59a36c00886edb33019a0337a1632d933abcff0b
sum <guo0808@emails.bjut.edu.cn>
2025-02-26 20:48:54 +0800

    make assert right

--- source/tests/pt/test_weighted_avg.py
+++ source/tests/pt/test_weighted_avg.py
@@ -70,11 +70,11 @@ class Test_testener_without_spin(unittest.TestCase):
             has_atom_ener=False,
         )
         self.assertIn("mae_e", err, "'mae_e' key is missing in the result")
-        self.assertNotIn("mae_fm", err, "'mae_fm' key is missing in the result")
+        self.assertNotIn("mae_fm", err, "'mae_fm' key should not be present in the result")
         self.assertNotIn(
             "mae_v", err, "'mae_v' key should not be present in the result"
         )
-        self.assertIn("mae_f", err, "'mae_f' key should not be present in the result")
+        self.assertIn("mae_f", err, "'mae_f' key is missing in the result")
 
         os.unlink(self.tmp_model.name)
 
@@ -135,9 +135,9 @@ class Test_testener_with_virial(unittest.TestCase):
             has_atom_ener=False,
         )
         self.assertIn("mae_e", err, "'mae_e' key is missing in the result")
-        self.assertNotIn("mae_fm", err, "'mae_fm' key is missing in the result")
-        self.assertIn("mae_v", err, "'mae_v' key should not be present in the result")
-        self.assertIn("mae_f", err, "'mae_f' key should not be present in the result")
+        self.assertNotIn("mae_fm", err, "'mae_fm' key should not missing in the result")
+        self.assertIn("mae_v", err, "'mae_v' key is missing in the result")
+        self.assertIn("mae_f", err, "'mae_f' key is missing in the result")
         os.unlink(self.tmp_model.name)
 
     def tearDown(self) -> None:
