2580af87f99c508e529d1631a7a7ee026fe1c894
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2025-02-27 17:50:52 +0800

    docs: add v3 paper citations (#4619)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Documentation**
    - Added a new citation for the "DeePMD-kit v3" article in various
    documentation files.
    - Clarified references to distinguish between the "v2" and "v3"
    publications.
      - Enhanced bibliography and credits details for improved clarity.
    
    - **Chores**
    - Revised citation formatting across the package to reflect formal
    publication standards.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@ustc.edu.cn>
    Signed-off-by: Jinzhe Zeng <njzjz@qq.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    (cherry picked from commit 918d4deb78edfeb95ddcd74b1fb50c213be55689)

--- deepmd/utils/summary.py
+++ deepmd/utils/summary.py
@@ -42,6 +42,7 @@ class SummaryPrinter(ABC):
         "Please read and cite:",
         "Wang, Zhang, Han and E, Comput.Phys.Comm. 228, 178-184 (2018)",
         "Zeng et al, J. Chem. Phys., 159, 054801 (2023)",
+        "Zeng et al, arxiv:2502.19161",
         "See https://deepmd.rtfd.io/credits/ for details.",
     )
 
