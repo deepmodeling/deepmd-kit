6d6c3fda571eb1d89deab664ca4eb1e40c37dbda
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2025-03-01 15:30:39 +0800

    fix(array-api): fix xp.where errors (#4624)
    `xp.where` always requires a bool array as its first input, but
    previously, the array-api-strict package didn't require it.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Refactor**
    - Enhanced the internal filtering logic by standardizing type handling
    for exclusion conditions, ensuring more reliable and consistent
    operations across the system.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@ustc.edu.cn>
    (cherry picked from commit 05ba1bf7a9379987c792f8990a34dd7b2a9a6ab1)

--- deepmd/dpmodel/descriptor/dpa1.py
+++ deepmd/dpmodel/descriptor/dpa1.py
@@ -899,6 +899,7 @@ class DescrptBlockSeAtten(NativeOP, DescriptorBlock):
         exclude_mask = self.emask.build_type_exclude_mask(nlist, atype_ext)
         # nfnl x nnei
         exclude_mask = xp.reshape(exclude_mask, (nf * nloc, nnei))
+        exclude_mask = xp.astype(exclude_mask, xp.bool)
         # nfnl x nnei
         nlist = xp.reshape(nlist, (nf * nloc, nnei))
         nlist = xp.where(exclude_mask, nlist, xp.full_like(nlist, -1))
