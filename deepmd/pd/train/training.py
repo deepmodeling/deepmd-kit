76a0008e2599539f05270543db5278887d2e3ef0
SigureMo <sigure.qaq@gmail.com>
2025-03-21 11:03:38 +0800

    style(pd): run pre-commit

--- deepmd/pd/train/training.py
+++ deepmd/pd/train/training.py
@@ -636,13 +636,12 @@ class Trainer:
         if CINN:
             from paddle import (
                 jit,
-                static,
             )
 
             backend = "CINN" if CINN else None
-            self.wrapper.forward = jit.to_static(
-                full_graph=True, backend=backend
-            )(self.wrapper.forward)
+            self.wrapper.forward = jit.to_static(full_graph=True, backend=backend)(
+                self.wrapper.forward
+            )
             log.info(
                 "Enable CINN during training, there may be some additional "
                 "compilation time in the first traning step."
