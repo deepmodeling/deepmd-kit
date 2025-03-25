089995b4e2cbb55fd66923961459de95127f8110
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2025-03-25 21:25:54 +0800

    fix(jax): fix Hessian NaN for DPA-3 (#4668)
    This fixes the NaN when calculating the Hessian in the JAX backend.
    Reproduce NaN:
    ```py
    from deepmd.pt.utils.serialization import serialize_from_file
    from deepmd.jax.model.ener_model import EnergyModel
    from deepmd.jax.env import jax, jnp
    import numpy as np
    jax.config.update("jax_debug_nans", True)
    
    model = serialize_from_file('frozen_model.pth')
    model = EnergyModel.deserialize(model["model"])
    model.enable_hessian()
    model_call = jax.jit(model.call)
    # nframes x natoms x 3
    coord = np.array([[[0,0,0],[1,1,1]]], dtype=np.float64)
    # nframes x natoms
    atype = np.array([[0,1]], dtype=int)
    print(model_call(jnp.array(coord), jnp.array(atype), None)['energy_derv_r_derv_r'])
    ```
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Bug Fixes**
    - Improved numerical stability by enhancing the vector normalization
    process to better handle edge cases and ensure robust computations.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@ustc.edu.cn>

--- deepmd/dpmodel/descriptor/repformers.py
+++ deepmd/dpmodel/descriptor/repformers.py
@@ -28,6 +28,9 @@ from deepmd.dpmodel.utils.network import (
     NativeLayer,
     get_activation_fn,
 )
+from deepmd.dpmodel.utils.safe_gradient import (
+    safe_for_vector_norm,
+)
 from deepmd.dpmodel.utils.seed import (
     child_seed,
 )
@@ -414,7 +417,7 @@ class DescrptBlockRepformers(NativeOP, DescriptorBlock):
         if not self.direct_dist:
             g2, h2 = xp.split(dmatrix, [1], axis=-1)
         else:
-            g2, h2 = xp.linalg.vector_norm(diff, axis=-1, keepdims=True), diff
+            g2, h2 = safe_for_vector_norm(diff, axis=-1, keepdims=True), diff
             g2 = g2 / self.rcut
             h2 = h2 / self.rcut
         # nf x nloc x nnei x ng2
