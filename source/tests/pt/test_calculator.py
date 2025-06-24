e8a68035c123f58b55fdddaee40617eed88088e8
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2025-06-24 16:21:37 +0800

    feat: support a/fparam in DP calculator

--- source/tests/pt/test_calculator.py
+++ source/tests/pt/test_calculator.py
@@ -102,3 +102,88 @@ class TestCalculator(unittest.TestCase):
         np.testing.assert_allclose(f0[idx_perm, :], f1, rtol=low_prec, atol=prec)
         np.testing.assert_allclose(s0, s1, rtol=low_prec, atol=prec)
         np.testing.assert_allclose(v0, v1, rtol=low_prec, atol=prec)
+
+
+
+class TestCalculatorWithFparamAparam(unittest.TestCase):
+    def setUp(self) -> None:
+        input_json = str(Path(__file__).parent / "water/se_atten.json")
+        with open(input_json) as f:
+            self.config = json.load(f)
+        self.config["training"]["numb_steps"] = 1
+        self.config["model"]["fitting_net"]["numb_fparam"] = 2
+        self.config["model"]["fitting_net"]["numb_aparam"] = 1
+        self.config["training"]["save_freq"] = 1
+        data_file = [str(Path(__file__).parent / "water/data/data_0")]
+        self.config["training"]["training_data"]["systems"] = data_file
+        self.config["training"]["validation_data"]["systems"] = [
+            str(Path(__file__).parent / "water/data/single")
+        ]
+        self.input_json = "test_dp_test.json"
+        with open(self.input_json, "w") as fp:
+            json.dump(self.config, fp, indent=4)
+
+        trainer = get_trainer(deepcopy(self.config))
+        trainer.run()
+
+        with torch.device("cpu"):
+            input_dict, label_dict, _ = trainer.get_data(is_train=False)
+        _, _, more_loss = trainer.wrapper(**input_dict, label=label_dict, cur_lr=1.0)
+
+        self.calculator = DPCalculator("model.pt")
+
+    def test_calculator(self) -> None:
+        from ase import (
+            Atoms,
+        )
+
+        natoms = 5
+        cell = torch.eye(3, dtype=dtype, device="cpu") * 10
+        generator = torch.Generator(device="cpu").manual_seed(GLOBAL_SEED)
+        coord = torch.rand([natoms, 3], dtype=dtype, device="cpu", generator=generator)
+        coord = torch.matmul(coord, cell)
+        fparam = torch.IntTensor([1, 2])
+        aparam = torch.IntTensor([[1], [0], [2], [1], [0]])
+        atomic_numbers = [1, 1, 1, 8, 8]
+        idx_perm = [1, 0, 4, 3, 2]
+
+        prec = 1e-10
+        low_prec = 1e-4
+
+        ase_atoms0 = Atoms(
+            numbers=atomic_numbers,
+            positions=coord,
+            # positions=[tuple(item) for item in coordinate],
+            cell=cell,
+            calculator=self.calculator,
+            pbc=True,
+        )
+        ase_atoms0.info.update({"fparam": fparam, "aparam": aparam})
+        e0, f0 = ase_atoms0.get_potential_energy(), ase_atoms0.get_forces()
+        s0, v0 = (
+            ase_atoms0.get_stress(voigt=True),
+            -ase_atoms0.get_stress(voigt=False) * ase_atoms0.get_volume(),
+        )
+
+        ase_atoms1 = Atoms(
+            numbers=[atomic_numbers[i] for i in idx_perm],
+            positions=coord[idx_perm, :],
+            # positions=[tuple(item) for item in coordinate],
+            cell=cell,
+            calculator=self.calculator,
+            pbc=True,
+        )
+        ase_atoms1.info.update({"fparam": fparam, "aparam": aparam[idx_perm, :]})
+        e1, f1 = ase_atoms1.get_potential_energy(), ase_atoms1.get_forces()
+        s1, v1 = (
+            ase_atoms1.get_stress(voigt=True),
+            -ase_atoms1.get_stress(voigt=False) * ase_atoms1.get_volume(),
+        )
+
+        assert isinstance(e0, float)
+        assert f0.shape == (natoms, 3)
+        assert v0.shape == (3, 3)
+        np.testing.assert_allclose(e0, e1, rtol=low_prec, atol=prec)
+        np.testing.assert_allclose(f0[idx_perm, :], f1, rtol=low_prec, atol=prec)
+        np.testing.assert_allclose(s0, s1, rtol=low_prec, atol=prec)
+        np.testing.assert_allclose(v0, v1, rtol=low_prec, atol=prec)
