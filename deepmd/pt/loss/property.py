8dae2fa94c3395585cca430ba23e22ce9e952591
SchrodingersCattt <gmy721212@163.com>
2025-07-29 12:34:41 +0000

    feat: add Mean absolute percentage error (MAPE) loss for property prediction

--- deepmd/pt/loss/property.py
+++ deepmd/pt/loss/property.py
@@ -42,7 +42,7 @@ class PropertyLoss(TaskLoss):
         var_name : str
             The atomic property to fit, 'energy', 'dipole', and 'polar'.
         loss_func : str
-            The loss function, such as "smooth_mae", "mae", "rmse".
+            The loss function, such as "smooth_mae", "mae", "rmse", "mape".
         metric : list
             The metric such as mae, rmse which will be printed.
         beta : float
@@ -151,6 +151,12 @@ class PropertyLoss(TaskLoss):
                     reduction="mean",
                 )
             )
+        elif self.loss_func == "mape":
+            loss += torch.mean(
+                torch.abs(
+                    (label[var_name] - model_pred[var_name]) / (label[var_name] + 1e-3)
+                )
+            )
         else:
             raise RuntimeError(f"Unknown loss function : {self.loss_func}")
 
@@ -182,6 +188,11 @@ class PropertyLoss(TaskLoss):
                     reduction="mean",
                 )
             ).detach()
+        if "mape" in self.metric:
+            more_loss[f"mape"] = torch.mean(torch.abs(
+                    (label[var_name] - model_pred[var_name]) /( label[var_name] + 1e-3) 
+                )
+            ) 
 
         return model_pred, loss, more_loss
 
