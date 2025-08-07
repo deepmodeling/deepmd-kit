b57a3aba7f83ff682db1b34c03cfedff8453d803
Chengqian Zhang <2000011006@stu.pku.edu.cn>
2025-08-07 11:04:08 +0000

    Delete test_dp_test part

--- source/tests/pt/test_dp_test.py
+++ source/tests/pt/test_dp_test.py
@@ -187,27 +187,6 @@ class TestDPTestPropertySeA(unittest.TestCase):
             to_numpy_array(result[model.get_var_name()])[0],
         )
 
-    def test_dp_test_padding_atoms(self) -> None:
-        trainer = get_trainer(deepcopy(self.config))
-        with torch.device("cpu"):
-            input_dict, label_dict, _ = trainer.get_data(is_train=False)
-        input_dict.pop("spin", None)
-        result = trainer.model(**input_dict)
-        padding_atoms_list = [1, 5, 10]
-        for padding_atoms in padding_atoms_list:
-            input_dict_padding = deepcopy(input_dict)
-            input_dict_padding["atype"] = F.pad(
-                input_dict_padding["atype"], (0, padding_atoms), value=-1
-            )
-            input_dict_padding["coord"] = F.pad(
-                input_dict_padding["coord"], (0, 0, 0, padding_atoms, 0, 0), value=0
-            )
-            result_padding = trainer.model(**input_dict_padding)
-            np.testing.assert_almost_equal(
-                to_numpy_array(result[trainer.model.get_var_name()])[0],
-                to_numpy_array(result_padding[trainer.model.get_var_name()])[0],
-            )
-
     def tearDown(self) -> None:
         for f in os.listdir("."):
             if f.startswith("model") and f.endswith(".pt"):
