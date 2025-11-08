cf93f90d5cc01a28d2de204c21217286587e2960
Chengqian Zhang <2000011006@stu.pku.edu.cn>
2025-11-08 10:19:46 +0000

    delete jit

--- deepmd/pt/model/model/make_model.py
+++ deepmd/pt/model/model/make_model.py
@@ -531,7 +531,6 @@ def make_model(T_AtomicModel: type[BaseAtomicModel]) -> type:
             """Check if the model has default frame parameters."""
             return self.atomic_model.has_default_fparam()
 
-        @torch.jit.export
         def get_default_fparam(self) -> Optional[torch.Tensor]:
             return self.atomic_model.get_default_fparam()
 
