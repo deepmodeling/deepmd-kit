7c82e85f96aa672c541116bed3a8679a869d2ce7
anyangml <anyangpeng.ca@gmail.com>
2025-12-04 08:44:25 +0000

    fix: overwirte add coord type_sel

--- deepmd/entrypoints/test.py
+++ deepmd/entrypoints/test.py
@@ -1134,8 +1134,17 @@ def test_polar(
         atomic=atomic,
         must=True,
         high_prec=False,
-        # type_sel=dp.get_sel_type(),
+        type_sel=dp.get_sel_type(),
     )
+    # for TF compatibility, need to add type_sel to coord, so that the atom order is aligned with the label
+    data.add(
+        "coord", 
+        3, 
+        atomic=True, 
+        must=True,
+        type_sel=dp.get_sel_type(),
+    )
+
 
     test_data = data.get_test()
     polar, numb_test, atype = run_test(dp, test_data, numb_test, data)
@@ -1275,7 +1284,15 @@ def test_dipole(
         atomic=atomic,
         must=True,
         high_prec=False,
-        # type_sel=dp.get_sel_type(),
+        type_sel=dp.get_sel_type(),
+    )
+    # for TF compatibility, need to add type_sel to coord, so that the atom order is aligned with the label
+    data.add(
+        "coord", 
+        3, 
+        atomic=True, 
+        must=True,
+        type_sel=dp.get_sel_type(),
     )
     test_data = data.get_test()
     dipole, numb_test, atype = run_test(dp, test_data, numb_test, data)
