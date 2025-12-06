59c0eb84e4e386d85e72ecf5ae695a08e29f403d
Jinzhe Zeng <jinzhe.zeng@ustc.edu.cn>
2025-12-06 18:36:43 +0800

    import paddle before tensorflow

--- source/tests/__init__.py
+++ source/tests/__init__.py
@@ -1,1 +1,7 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
+
+import os
+
+if os.environ.get("DP_CI_IMPORT_PADDLE_BEFORE_TF", "0") == "1":
+    import paddle as _  # noqa: F401
+    import tensorflow as _  # noqa: F401
