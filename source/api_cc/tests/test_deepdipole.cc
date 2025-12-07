61491bb1537c417cc8bbff03727c3ac496a581d3
Jinzhe Zeng <jinzhe.zeng@ustc.edu.cn>
2025-12-08 01:32:30 +0800

    add skip

--- source/api_cc/tests/test_deepdipole.cc
+++ source/api_cc/tests/test_deepdipole.cc
@@ -30,6 +30,9 @@ class TestInferDeepDipole : public ::testing::Test {
   deepmd::DeepTensor dp;
 
   void SetUp() override {
+#ifndef BUILD_TENSORFLOW
+    GTEST_SKIP() << "Skip because TensorFlow support is not enabled.";
+#endif
     deepmd::convert_pbtxt_to_pb("../../tests/infer/deepdipole.pbtxt",
                                 "deepdipole.pb");
 
@@ -441,6 +444,9 @@ class TestInferDeepDipoleFake : public ::testing::Test {
   deepmd::DeepTensor dp;
 
   void SetUp() override {
+#ifndef BUILD_TENSORFLOW
+    GTEST_SKIP() << "Skip because TensorFlow support is not enabled.";
+#endif
     deepmd::convert_pbtxt_to_pb("../../tests/infer/deepdipole_fake.pbtxt",
                                 "deepdipole_fake.pb");
 
