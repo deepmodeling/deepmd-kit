ed7af14d384b470570ebcb1d4c3b678dd99e7344
OutisLi <LTC201806070316@gmail.com>
2026-01-23 16:05:08 +0800

    fix tf model for virial and update UT results.

--- source/lmp/tests/test_lammps_spin.py
+++ source/lmp/tests/test_lammps_spin.py
@@ -9,6 +9,7 @@ from pathlib import (
     Path,
 )
 
+import constants
 import numpy as np
 import pytest
 from lammps import (
@@ -70,6 +71,124 @@ expected_fm2 = np.array(
         [0.0000000000000000, 0.00000000000000000, 0.00000000000000000],
     ]
 )
+
+expected_v = -np.array(
+    [
+        0.0070639867264982,
+        -0.0005923577001662,
+        -0.0015491268442953,
+        -0.0005741900039506,
+        0.0004072991754844,
+        0.0005919446476345,
+        -0.0013659665914274,
+        0.0005245686552392,
+        0.0011288634277803,
+        0.0074611996305919,
+        -0.0015158254500315,
+        -0.0030704181444311,
+        -0.0015503527871207,
+        0.0006417155838534,
+        0.0010901024672963,
+        -0.0032762727340245,
+        0.0011481000769186,
+        0.0022122852076016,
+        -0.0049637269273085,
+        -0.0033079530214069,
+        0.0048850199723435,
+        -0.0032277537906931,
+        -0.0030526361938397,
+        0.0044721003136312,
+        0.0053457625015160,
+        0.0044600355962439,
+        -0.0065441506206723,
+        -0.0044231868209291,
+        -0.0033953486551904,
+        0.0050014995082810,
+        -0.0035584060948890,
+        -0.0032308004485022,
+        0.0047399657455500,
+        0.0056902937417672,
+        0.0047696802946761,
+        -0.0070004831270587,
+        0.0034978220789713,
+        -0.0044217265408896,
+        -0.0075771507215158,
+        -0.0043265981217727,
+        0.0016344211766637,
+        0.0031438764476946,
+        -0.0069613658908443,
+        0.0032277030414985,
+        0.0055466693735168,
+        -0.0182670501038624,
+        -0.0030197903610554,
+        0.0012333318415169,
+        -0.0030157009303137,
+        0.0006787737562374,
+        0.0017594542103399,
+        0.0025814653441594,
+        0.0020137939338955,
+        0.0014966802677115,
+    ]
+).reshape(6, 9)
+
+expected_v2 = -np.array(
+    [
+        -0.0068361570854045,
+        0.0013367399255969,
+        0.0027254156851031,
+        0.0013170622611582,
+        -0.0006584860372994,
+        -0.0011202746253630,
+        0.0034372788237214,
+        -0.0014078563891643,
+        -0.0026234422772135,
+        -0.0065741762148638,
+        0.0017805968136400,
+        0.0034294170220393,
+        0.0017765463870965,
+        -0.0007431139812367,
+        -0.0013178346591454,
+        0.0028404497247166,
+        -0.0010584927243460,
+        -0.0019736773403821,
+        -0.0005990686037429,
+        -0.0006790752112473,
+        0.0009843156545704,
+        -0.0006499258389702,
+        -0.0005944102287950,
+        0.0008730050519947,
+        0.0009534901617246,
+        0.0008816224750390,
+        -0.0012893921034252,
+        -0.0004768372014215,
+        -0.0006613274582292,
+        0.0009616568976184,
+        -0.0007221357003557,
+        -0.0006885045030101,
+        0.0010101324669343,
+        0.0010319090966831,
+        0.0009955172109546,
+        -0.0014553634659113,
+        0.0027303678059308,
+        -0.0017755948961346,
+        -0.0030761923779883,
+        -0.0017183308924552,
+        0.0006816964863724,
+        0.0012957090028794,
+        -0.0042486220709583,
+        0.0018539755349361,
+        0.0032531905210736,
+        -0.0053263906035893,
+        -0.0013059666848022,
+        -0.0000753225555805,
+        -0.0013078437276500,
+        0.0003423965327707,
+        0.0008080454760442,
+        0.0003139988733780,
+        0.0008955147923178,
+        0.0008788489971600,
+    ]
+).reshape(6, 9)
 
 box = np.array([0, 13, 0, 13, 0, 13, 0, 0, 0])
 coord = np.array(
@@ -161,15 +280,15 @@ def test_pair_deepmd_virial(lammps) -> None:
     lammps.compute("pressure all pressure NULL pair")
     lammps.compute("virial all centroid/stress/atom NULL pair")
     lammps.variable("eatom atom c_peatom")
-    # for ii in range(9):
-    #     jj = [0, 4, 8, 3, 6, 7, 1, 2, 5][ii]
-    #     lammps.variable(f"pressure{jj} equal c_pressure[{ii+1}]")
-    # for ii in range(9):
-    #     jj = [0, 4, 8, 3, 6, 7, 1, 2, 5][ii]
-    #     lammps.variable(f"virial{jj} atom c_virial[{ii+1}]")
-    # lammps.dump(
-    #     "1 all custom 1 dump id " + " ".join([f"v_virial{ii}" for ii in range(9)])
-    # )
+    for ii in range(9):
+        jj = [0, 4, 8, 3, 6, 7, 1, 2, 5][ii]
+        lammps.variable(f"pressure{jj} equal c_pressure[{ii + 1}]")
+    for ii in range(9):
+        jj = [0, 4, 8, 3, 6, 7, 1, 2, 5][ii]
+        lammps.variable(f"virial{jj} atom c_virial[{ii + 1}]")
+    lammps.dump(
+        "1 all custom 1 dump id " + " ".join([f"v_virial{ii}" for ii in range(9)])
+    )
     lammps.run(0)
     assert lammps.eval("pe") == pytest.approx(expected_e)
     for ii in range(4):
@@ -180,18 +299,16 @@ def test_pair_deepmd_virial(lammps) -> None:
     assert np.array(lammps.variables["eatom"].value) == pytest.approx(
         expected_ae[idx_map]
     )
-    # vol = box[1] * box[3] * box[5]
-    # for ii in range(6):
-    #     jj = [0, 4, 8, 3, 6, 7, 1, 2, 5][ii]
-    #     assert np.array(
-    #         lammps.variables[f"pressure{jj}"].value
-    #     ) / constants.nktv2p == pytest.approx(
-    #         -expected_v[idx_map, jj].sum(axis=0) / vol
-    #     )
-    # for ii in range(9):
-    #     assert np.array(
-    #         lammps.variables[f"virial{ii}"].value
-    #     ) / constants.nktv2p == pytest.approx(expected_v[idx_map, ii])
+    vol = box[1] * box[3] * box[5]
+    for ii in range(6):
+        jj = [0, 4, 8, 3, 6, 7, 1, 2, 5][ii]
+        assert np.array(
+            lammps.variables[f"pressure{jj}"].value
+        ) / constants.nktv2p == pytest.approx(-expected_v[:, jj].sum(axis=0) / vol)
+    for ii in range(9):
+        assert np.array(
+            lammps.variables[f"virial{ii}"].value
+        ) / constants.nktv2p == pytest.approx(expected_v[idx_map, ii])
 
 
 def test_pair_deepmd_model_devi(lammps) -> None:
@@ -215,6 +332,13 @@ def test_pair_deepmd_model_devi(lammps) -> None:
     assert md[7] == pytest.approx(np.max(expected_md_fm))
     assert md[8] == pytest.approx(np.min(expected_md_fm))
     assert md[9] == pytest.approx(np.mean(expected_md_fm))
+    expected_md_v = (
+        np.std([np.sum(expected_v[:], axis=0), np.sum(expected_v2[:], axis=0)], axis=0)
+        / 4
+    )
+    assert md[1] == pytest.approx(np.max(expected_md_v))
+    assert md[2] == pytest.approx(np.min(expected_md_v))
+    assert md[3] == pytest.approx(np.sqrt(np.mean(np.square(expected_md_v))))
 
 
 def test_pair_deepmd_model_devi_atomic_relative(lammps) -> None:
@@ -243,6 +367,42 @@ def test_pair_deepmd_model_devi_atomic_relative(lammps) -> None:
     assert md[7] == pytest.approx(np.max(expected_md_fm))
     assert md[8] == pytest.approx(np.min(expected_md_fm))
     assert md[9] == pytest.approx(np.mean(expected_md_fm))
+
+
+def test_pair_deepmd_model_devi_atomic_relative_v(lammps) -> None:
+    relative = 1.0
+    lammps.pair_style(
+        f"deepspin {pb_file.resolve()} {pb_file2.resolve()} out_file {md_file.resolve()} out_freq 1 atomic relative_v {relative}"
+    )
+    lammps.pair_coeff("* *")
+    lammps.run(0)
+    assert lammps.eval("pe") == pytest.approx(expected_e)
+    for ii in range(4):
+        assert lammps.atoms[ii].force == pytest.approx(
+            expected_f[lammps.atoms[ii].id - 1]
+        )
+    md = np.loadtxt(md_file.resolve())
+    expected_md_f = np.linalg.norm(np.std([expected_f, expected_f2], axis=0), axis=1)
+    expected_md_fm = np.linalg.norm(np.std([expected_fm, expected_fm2], axis=0), axis=1)
+    assert md[4] == pytest.approx(np.max(expected_md_f))
+    assert md[5] == pytest.approx(np.min(expected_md_f))
+    assert md[6] == pytest.approx(np.mean(expected_md_f))
+    assert md[7] == pytest.approx(np.max(expected_md_fm))
+    assert md[8] == pytest.approx(np.min(expected_md_fm))
+    assert md[9] == pytest.approx(np.mean(expected_md_fm))
+    expected_md_v = (
+        np.std([np.sum(expected_v, axis=0), np.sum(expected_v2, axis=0)], axis=0) / 4
+    )
+    norm = (
+        np.abs(
+            np.mean([np.sum(expected_v, axis=0), np.sum(expected_v2, axis=0)], axis=0)
+        )
+        / 4
+    )
+    expected_md_v /= norm + relative
+    assert md[1] == pytest.approx(np.max(expected_md_v))
+    assert md[2] == pytest.approx(np.min(expected_md_v))
+    assert md[3] == pytest.approx(np.sqrt(np.mean(np.square(expected_md_v))))
 
 
 @pytest.mark.skipif(
