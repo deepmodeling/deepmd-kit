c65b740629e4d1a4e85d87ff13856f975807fe03
OutisLi <LTC201806070316@gmail.com>
2026-01-26 16:38:58 +0800

    1

--- deepmd/utils/argcheck.py
+++ deepmd/utils/argcheck.py
@@ -3349,7 +3349,7 @@ def training_args(
             ],
         ),
         Argument(
-            "num_epoch",
+            "numb_epoch",
             [int, float],
             optional=True,
             doc=doc_num_epoch,
