b7af468e7f2d5a711c16e9a84e071caecc1af685
Han Wang <wang_han@iapcm.ac.cn>
2026-02-25 12:28:52 +0800

    chore(pd): update in the same way as pt

--- deepmd/pd/train/training.py
+++ deepmd/pd/train/training.py
@@ -1417,6 +1417,13 @@ def model_change_out_bias(
     )
     new_bias = deepcopy(_model.get_out_bias())
 
+    from deepmd.pd.model.model.dp_model import (
+        DPModelCommon,
+    )
+
+    if isinstance(_model, DPModelCommon) and _bias_adjust_mode == "set-by-statistic":
+        _model.get_fitting_net().compute_input_stats(_sample_func)
+
     model_type_map = _model.get_type_map()
     log.info(
         f"Change output bias of {model_type_map!s} "
