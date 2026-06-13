c558c1447293ff44a7f17c7f3305760f8b4cae63
Jinzhe Zeng <jinzhe.zeng@ustc.edu.cn>
2026-06-13 23:34:12 +0800

    refactor(tests): auto-convert array-api-strict modules

--- source/tests/array_api_strict/descriptor/se_t_tebd.py
+++ source/tests/array_api_strict/descriptor/se_t_tebd.py
@@ -1,47 +1,22 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
-from typing import (
-    Any,
-)
-
 from deepmd.dpmodel.descriptor.se_t_tebd import (
     DescrptBlockSeTTebd as DescrptBlockSeTTebdDP,
 )
 from deepmd.dpmodel.descriptor.se_t_tebd import DescrptSeTTebd as DescrptSeTTebdDP
 
 from ..common import (
-    to_array_api_strict_array,
+    array_api_strict_module,
 )
-from ..utils.exclude_mask import (
-    PairExcludeMask,
-)
-from ..utils.network import (
-    NetworkCollection,
-)
-from ..utils.type_embed import (
-    TypeEmbedNet,
-)
+from ..utils import exclude_mask as _strict_exclude_mask  # noqa: F401
+from ..utils import network as _strict_network  # noqa: F401
+from ..utils import type_embed as _strict_type_embed  # noqa: F401
 
 
+@array_api_strict_module
 class DescrptBlockSeTTebd(DescrptBlockSeTTebdDP):
-    def __setattr__(self, name: str, value: Any) -> None:
-        if name in {"mean", "stddev"}:
-            value = to_array_api_strict_array(value)
-        elif name in {"embeddings", "embeddings_strip"}:
-            if value is not None:
-                value = NetworkCollection.deserialize(value.serialize())
-        elif name == "env_mat":
-            # env_mat doesn't store any value
-            pass
-        elif name == "emask":
-            value = PairExcludeMask(value.ntypes, value.exclude_types)
-
-        return super().__setattr__(name, value)
+    pass
 
 
+@array_api_strict_module
 class DescrptSeTTebd(DescrptSeTTebdDP):
-    def __setattr__(self, name: str, value: Any) -> None:
-        if name == "se_ttebd":
-            value = DescrptBlockSeTTebd.deserialize(value.serialize())
-        elif name == "type_embedding":
-            value = TypeEmbedNet.deserialize(value.serialize())
-        return super().__setattr__(name, value)
+    pass
