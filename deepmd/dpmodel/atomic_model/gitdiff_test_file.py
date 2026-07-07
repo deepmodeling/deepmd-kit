093ce3e48f57dede2ae605a24283faafd5a9ea17
Gongxl972 <141223555+Gongxl972@users.noreply.github.com>
2026-07-07 10:13:13 +0800

    Add files via upload
    Signed-off-by: Gongxl972 <141223555+Gongxl972@users.noreply.github.com>

---
+++ deepmd/dpmodel/atomic_model/gitdiff_test_file.py
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+from typing import (
+    Any,
+)
+
+from deepmd.dpmodel.fitting.ener_fitting import (
+    EnergyFittingNet,
+    InvarFitting,
+)
+
+from .dp_atomic_model import (
+    DPAtomicModel,
+)
+
+
+#class DPEnergyAtomicModel(DPAtomicModel):
+    def __init__(
+        self, descriptor: Any, fitting: Any, type_map: list[str], **kwargs: Any
+    ) -> None:
+        if not (
+            isinstance(fitting, EnergyFittingNet) or isinstance(fitting, InvarFitting)
+        ):
+            raise TypeError(
+                "fitting must be an instance of EnergyFittingNet or InvarFitting for DPEnergyAtomicModel"
+            )
+        super().__init__(descriptor, fitting, type_map, **kwargs)
+
